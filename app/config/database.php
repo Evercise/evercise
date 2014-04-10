eff848e883ac1ae4f7e3e6d3ac8ed66386f32e7c
tristanallen <furryfool@gmail.com>
2014-04-10 10:57:05 +0100

    Updated database name "evercise_2_0"

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'evercise-laravel',
+			'database'  => 'evercise_2_0',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
