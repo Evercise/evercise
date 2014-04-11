60242b3ffd53e254ef80b440c87e4c513ea3da52
Lewis Bayfield <bassymew@gmail.com>
2014-04-11 15:48:34 +0100

    setting up pipeline

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -2,16 +2,18 @@
 <html>
 <head>
 	<title>Master template</title>
+
 	{{ stylesheet_link_tag() }}
 	{{ javascript_include_tag() }}
-	<script type="text/javascript">
-	     less.env = "development";
-	     less.watch();
-	</script>
+
+
+	
 </head>
 <body>
 	<div class="container">
+
 		@yield('content')
+		
 	</div>
 </body>
 </html>
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -25,7 +25,7 @@ $app = new Illuminate\Foundation\Application;
 */
 
 $env = $app->detectEnvironment(array(
-  'local' => array('localhost'),
+'local' => array('*Tris*','*Mewis*'),
 ));
 
 /*
