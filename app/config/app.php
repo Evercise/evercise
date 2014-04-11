54d89d1388fb2655f4922381e48e57b85ba6186f
Lewis Bayfield <bassymew@gmail.com>
2014-04-11 12:23:32 +0100

    added assets

--- app/config/app.php
+++ app/config/app.php
@@ -107,6 +107,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
+		'Codesleeve\AssetPipeline\AssetPipelineServiceProvider',
 
 	),
 
