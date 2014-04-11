75a86953a278dcf1cc498cf1b5525417b326efbf
tristanallen <furryfool@gmail.com>
2014-04-11 15:47:43 +0100

    Fixed local dev mode support

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -4,10 +4,6 @@
 	<title>Master template</title>
 	{{ stylesheet_link_tag() }}
 	{{ javascript_include_tag() }}
-	<script type="text/javascript">
-	     less.env = "development";
-	     less.watch();
-	</script>
 </head>
 <body>
 	<div class="container">
