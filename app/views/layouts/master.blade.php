7e38be12175c120258767b3eec4c6babee6bf06d
tristanallen <furryfool@gmail.com>
2014-04-11 13:35:23 +0100

    added LESS dev mode script (hopefully)

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -4,6 +4,10 @@
 	<title>Master template</title>
 	{{ stylesheet_link_tag() }}
 	{{ javascript_include_tag() }}
+	<script type="text/javascript">
+	     less.env = "development";
+	     less.watch();
+	</script>
 </head>
 <body>
 	<div class="container">
