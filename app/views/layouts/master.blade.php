4ebca526ed2e2a2f93648dce421d864bed59fbee
Lewis Bayfield <bassymew@gmail.com>
2014-04-15 15:22:57 +0100

    less css framework and css for user signup

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 	<title>Master template</title>
+	<link href='http://fonts.googleapis.com/css?family=Exo:300,400,500,600,300italic,400italic,500italic,600italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
 
 	{{ stylesheet_link_tag() }}
 	{{ javascript_include_tag() }}
