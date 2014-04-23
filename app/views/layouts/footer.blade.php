66aa171725cb3a11f123b570c25da951a98d7c9d
Lewis Bayfield <bassymew@gmail.com>
2014-04-23 13:43:30 +0100

    changed footer css

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -25,11 +25,10 @@
 			{{ HTML::linkRoute('auth.login', 'Discover fitness classes') }}
 		</aside>
 		<div>
-			<p>Follow us   &nbsp;
-				<a href="https://www.facebook.com/evercise"><img src="/img/facebook.png" alt="evercise facebook page"/></a>
-				<a href="https://twitter.com/LetsEvercise"><img src="/img/twitter.png" alt="evercise twitter page"/></a>
-				<a href="https://plus.google.com/+Evercisefitness"><img src="/img/googleplus.png" alt="evercise google plus page"/></a>
-			</p>
+			<p>Follow us</p>
+			<a href="https://www.facebook.com/evercise"><img src="/img/facebook.png" alt="evercise facebook page"/></a>
+			<a href="https://twitter.com/LetsEvercise"><img src="/img/twitter.png" alt="evercise twitter page"/></a>
+			<a href="https://plus.google.com/+Evercisefitness"><img src="/img/googleplus.png" alt="evercise google plus page"/></a>	
 		</div>
 	</section>
 	
