858f940d3a6b3ab8ef32923b7d54b14121eab3cd
Lewis Bayfield <bassymew@gmail.com>
2014-04-23 13:52:03 +0100

    added target blank to social links

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -26,9 +26,9 @@
 		</aside>
 		<div>
 			<p>Follow us</p>
-			<a href="https://www.facebook.com/evercise"><img src="/img/facebook.png" alt="evercise facebook page"/></a>
-			<a href="https://twitter.com/LetsEvercise"><img src="/img/twitter.png" alt="evercise twitter page"/></a>
-			<a href="https://plus.google.com/+Evercisefitness"><img src="/img/googleplus.png" alt="evercise google plus page"/></a>	
+			<a href="https://www.facebook.com/evercise" target="_blank"><img src="/img/facebook.png" alt="evercise facebook page"/></a>
+			<a href="https://twitter.com/LetsEvercise" target="_blank"><img src="/img/twitter.png" alt="evercise twitter page"/></a>
+			<a href="https://plus.google.com/+Evercisefitness" target="_blank"><img src="/img/googleplus.png" alt="evercise google plus page"/></a>	
 		</div>
 	</section>
 	
