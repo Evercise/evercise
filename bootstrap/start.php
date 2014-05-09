b88ed00212bcd53e8f1d2edface5c8b15d10aaa4
tristanallen <furryfool@gmail.com>
2014-05-09 10:07:59 +0100

    Any server name will do (test)

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
   'local' => array('*Tris*', '*Mewis*'),
-  'staging' => array('*avi*', '*donkey*', '*Donkey*'),
+  'staging' => array('*avi*', '*donkey*', '*Donkey*', '*'),
 ));
 
 /*
