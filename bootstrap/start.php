16aa2e93b1c6a696e5b26fbed8fca657383a647f
Lewis Bayfield <bassymew@gmail.com>
2014-05-09 09:53:08 +0100

    staging in start

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -25,7 +25,8 @@ $app = new Illuminate\Foundation\Application;
 */
 
 $env = $app->detectEnvironment(array(
-  'local' => array('*Tris*', '*Mewis*')
+  'local' => array('*Tris*', '*Mewis*'),
+  'staging' => array('*avi*', '*donkey*'),
 ));
 
 /*
