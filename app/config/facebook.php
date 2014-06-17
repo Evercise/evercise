49afc1f2c0cc28e071b13fe457a0b9767a3ff8dd
Lewis Bayfield <bassymew@gmail.com>
2014-06-17 17:51:49 +0100

    facebook conf donkey

--- app/config/facebook.php
+++ app/config/facebook.php
@@ -3,6 +3,8 @@
  
 // Facebook app Config 
 return array(
-        'appId' => '492492450851682',
-        'secret' => 'b5f9e533949178ebe7c33936c81ad833'
+        //'appId' => '492492450851682',
+        'appId' => '247621532113217', // donkey
+        //'secret' => 'b5f9e533949178ebe7c33936c81ad833'
+        'secret' => '762e0e54c435804033d7ece1d4b50122' // donkey
     );
