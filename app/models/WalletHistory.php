19e874a8f2b4866eecb3caf148bd8b3878743c91
tristanallen <furryfool@gmail.com>
2014-06-19 17:37:33 +0100

    update Wallethistory 1

--- app/models/WalletHistory.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Wallethistory extends \Eloquent {
-	protected $fillable = ['user_id', 'transaction_amount', 'new_balance', 'sessionpayment_id'];
-
-	protected $table = 'wallethistory';
-}
