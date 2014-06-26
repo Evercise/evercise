5d37839a59aee38b0350857d8ceffa1853250a23
tristanallen <furryfool@gmail.com>
2014-06-26 14:01:14 +0100

    more merge

--- app/composers/TrainerBlockComposer.php
+++ app/composers/TrainerBlockComposer.php
@@ -29,17 +29,9 @@ class TrainerBlockComposer {
   		$to   = new DateTime('today');
   		$age  =  $from->diff($to)->y;
 
-<<<<<<< HEAD
       $trainerDetails = $viewdata['trainer'];
   		$bio =  $trainerDetails->bio;
-      $speciality = Speciality::find($trainerDetails->specialities_id)->pluck(DB::raw("CONCAT(name, ' ', titles)"));
-=======
-
-  		$bio =  $trainerDetails->bio;
-
       //$speciality = Speciality::find($trainerDetails->specialities_id)->pluck(DB::raw("CONCAT(name, ' ', titles)"));
->>>>>>> 8da496fdd7c0f8b0c5c4ddfc8079d48490c109a2
-
       if ($orientation == 'landscape') {
         JavaScript::put(array('initReadMore' => 1 )); // Initialise read more.
       }
