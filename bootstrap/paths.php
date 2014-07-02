4e7af07f216e9a101537a21ef426c8db6521ffef
tristanallen <furryfool@gmail.com>
2014-07-02 10:09:48 +0100

    Un-messed paths

--- bootstrap/paths.php
+++ bootstrap/paths.php
@@ -3,12 +3,12 @@
 
 
 //http://stackoverflow.com/questions/16683046/how-to-install-laravel-4-to-a-web-host-subfolder-without-publicly-exposing-app
-return array(
+/*return array(
 	'app' => __DIR__.'/../../evercise/app',
 	'public' => __DIR__,
 	'base' => __DIR__.'/../../evercise',
 	'storage' => __DIR__.'/../../evercise/app/storage',
-);
+);*/
 
 
 return array(
