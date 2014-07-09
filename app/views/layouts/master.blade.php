b453ab54fefd14851a1f2125e6894f3189ba0259
Lewis Bayfield <bassymew@gmail.com>
2014-07-09 14:31:44 +0100

    centent type

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8"> 
 
 	<link href='http://fonts.googleapis.com/css?family=Exo:300,400,500,600,300italic,400italic,500italic,600italic' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
