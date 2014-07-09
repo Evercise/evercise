4f6cd32017c91c25d9cc3cd376fd5d99056388e6
Lewis Bayfield <bassymew@gmail.com>
2014-07-09 12:21:43 +0100

    map

--- app/assets/javascripts/widgets/map.js
+++ app/assets/javascripts/widgets/map.js
@@ -18,6 +18,7 @@ function MapWidgetInit() {
           dataType: 'json'
       })
       .done(
+        trace('done');
           function(data) { 
             trace(data);
             $('#latbox').val(data.lat);
