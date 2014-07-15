6edf328aa953be6205d9a8ceb620e9afde4585d1
Lewis Bayfield <bassymew@gmail.com>
2014-07-15 10:17:27 +0100

    text area

--- app/views/trainers/editForm.blade.php
+++ app/views/trainers/editForm.blade.php
@@ -2,17 +2,17 @@
 {{ Form::open(array('id' => 'trainer_create', 'url' => 'trainers/'.$trainer->id, 'method' => 'PUT', 'class' => 'create-form')) }}
 
 
-    @include('form.textfield', array('fieldname'=>'profession', 'placeholder'=>'Your profession', 'maxlength'=>50, 'label'=>'Add your profession', 'fieldtext'=>'Add your profession', 'default'=>$profession ))
+    @include('form.textarea', array('fieldname'=>'profession', 'placeholder'=>'Your profession', 'maxlength'=>50, 'label'=>'Add your profession', 'fieldtext'=>'Add your profession', 'default'=>$profession ))
     @if ($errors->has('profession'))
         {{ $errors->first('profession', '<p class="error-msg">:message</p>')}}
     @endif
 
-    @include('form.textfield', array('fieldarea'=>'bio', 'placeholder'=>'between 50 and 500 characters', 'maxlength'=>500, 'label'=>'Add your bio', 'fieldtext'=>'This is the bio that is displayed to our users', 'default'=>$bio ))
+    @include('form.textarea', array('fieldname'=>'bio', 'placeholder'=>'between 50 and 500 characters', 'maxlength'=>500, 'label'=>'Add your bio', 'fieldtext'=>'This is the bio that is displayed to our users', 'default'=>$bio ))
     @if ($errors->has('bio'))
         {{ $errors->first('bio', '<p class="error-msg">:message</p>')}}
     @endif
 
-    @include('form.textfield', array('fieldname'=>'website', 'placeholder'=>'website address', 'maxlength'=>100, 'label'=>'Add your website', 'fieldtext'=>'(Optional) - If you want users to learn more about you and what you do, you can include your web address, which will be visible on your profile.' ))
+    @include('form.textarea', array('fieldname'=>'website', 'placeholder'=>'website address', 'maxlength'=>100, 'label'=>'Add your website', 'fieldtext'=>'(Optional) - If you want users to learn more about you and what you do, you can include your web address, which will be visible on your profile.' ))
     @if ($errors->has('website'))
         {{ $errors->first('website', '<p class="error-msg">:message</p>')}}
     @endif
