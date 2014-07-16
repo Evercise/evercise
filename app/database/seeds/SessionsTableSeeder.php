e2469f0dd2d9ba9a76b713cf40aeafd52f84fba9
tristanallen <furryfool@gmail.com>
2014-07-16 20:16:06 +0100

    seeds

--- app/database/seeds/SessionsTableSeeder.php
+++ app/database/seeds/SessionsTableSeeder.php
@@ -25,32 +25,36 @@ class SessionsTableSeeder extends Seeder {
 
 			$classinfo = DB::connection('mysql_import')->table('classinfo')->where('classInfoId', $classdatetime->classInfoId)->first();
 
-			$evercisegroupId = DB::table('migrate_groups')->where('classInfoId', $classdatetime->classInfoId)->pluck('evercisegroup_id');
-
-			$evercisegroup = Evercisegroup::find($evercisegroupId);
-			if ($evercisegroup)
+			if($classinfo->classInfoPublished == 1)
 			{
 
-				$session = Evercisesession::create(array(
-					'evercisegroup_id'=>$evercisegroupId,
-					'date_time'=>$classdatetime->classDatetimeSlot,
-					'price'=>$classinfo->classInfoPrice,
-					'duration'=>$classinfo->classInfoDuration
-				));
+				$evercisegroupId = DB::table('migrate_groups')->where('classInfoId', $classdatetime->classInfoId)->pluck('evercisegroup_id');
 
-				$payment = Sessionpayment::create([
-					'user_id'=>$evercisegroup->user_id,
-					'evercisesession_id'=>$session->id,
-					'total'=>0,
-					'total_after_fees'=>0,
-					'commission'=>0,
-					'processed'=>1,
-				]);
+				$evercisegroup = Evercisegroup::find($evercisegroupId);
+				if ($evercisegroup)
+				{
 
-			}
-			else
-			{
-				$this->command->info('Cannot find evercisegroup. id: '.$evercisegroupId.', classinfo id: '.$classdatetime->classInfoId);
+					$session = Evercisesession::create(array(
+						'evercisegroup_id'=>$evercisegroupId,
+						'date_time'=>$classdatetime->classDatetimeSlot,
+						'price'=>$classinfo->classInfoPrice,
+						'duration'=>$classinfo->classInfoDuration
+					));
+
+					$payment = Sessionpayment::create([
+						'user_id'=>$evercisegroup->user_id,
+						'evercisesession_id'=>$session->id,
+						'total'=>0,
+						'total_after_fees'=>0,
+						'commission'=>0,
+						'processed'=>1,
+					]);
+
+				}
+				else
+				{
+					$this->command->info('Cannot find evercisegroup. id: '.$evercisegroupId.', classinfo id: '.$classdatetime->classInfoId);
+				}
 			}
 		}
 	}
