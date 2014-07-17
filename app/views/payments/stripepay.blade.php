99a8b488e20c59994ba09537a13bcf811472af80
tristanallen <furryfool@gmail.com>
2014-07-17 17:20:33 +0100

    stripe button out again

--- app/views/payments/stripepay.blade.php
+++ app/views/payments/stripepay.blade.php
@@ -3,5 +3,8 @@
 
 @section('content')
 
+<script src="https://checkout.stripe.com/v2/checkout.js" class="stripe-button"
+          data-key="@stripeKey"
+          data-amount="3" data-description="Evercise Sessions"></script>
 
 @stop
