8fe0ab4a3c3e17306a66f4ca5bf5f445bd09bd75
tristanallen <furryfool@gmail.com>
2014-07-17 18:27:01 +0100

    image crop bypass

--- app/views/widgets/crop.blade.php
+++ app/views/widgets/crop.blade.php
@@ -2,13 +2,13 @@
 <div id='img-crop'>
 	<img src='' alt='cropped-image'></img>
 
-	{{ Form::hidden( 'pos_x' , null, array('id' => 'pos_x')) }}
-	{{ Form::hidden( 'pos_y' , null, array('id' => 'pos_y')) }}
-	{{ Form::hidden( 'width' , null, array('id' => 'width')) }}
-	{{ Form::hidden( 'height' , null, array('id' => 'height')) }}
+	{{ Form::hidden( 'pos_x' , 0, array('id' => 'pos_x')) }}
+	{{ Form::hidden( 'pos_y' , 0, array('id' => 'pos_y')) }}
+	{{ Form::hidden( 'width' , 500, array('id' => 'width')) }}
+	{{ Form::hidden( 'height' , 500, array('id' => 'height')) }}
 	{{ Form::hidden( 'img_url' , null, array('id' => 'img_url')) }}
 	{{ Form::hidden( 'img_height' , null, array('id' => 'img_height')) }}
 
-	{{ Form::submit('Save' , array('class'=>'btn-yellow disabled')) }}
+	{{ Form::submit('Save' , array('class'=>'btn-yellow')) }}
         
 </div>
