31ca19288df5cdc723acaf101f296e5f30c2d9f8
tristanallen <furryfool@gmail.com>
2014-07-18 10:53:10 +0100

    testers can view testers classes

--- app/views/evercisegroups/discover_classes_block.blade.php
+++ app/views/evercisegroups/discover_classes_block.blade.php
@@ -2,10 +2,15 @@
 	@if (isset($evercisegroups)) 
 		@foreach ($evercisegroups as $key => $evercisegroup) 
 			<?php
-				 $sentryUser = Sentry::findUserById($evercisegroup->user->id);
-				 $admin = Sentry::findGroupById(5);
+				 $sentryUserGroup = Sentry::findUserById($evercisegroup->user->id);
+				 $sentryUser = Sentry::getUser();
+				 $testers = Sentry::findGroupById(5);
+
+				 $testerLoggedIn = false;
+				 if($sentryUser) $testerLoggedIn = $sentryUser->inGroup($testers);
+
 			 ?>
-			 @if (!$sentryUser->inGroup($admin))
+			 @if (!$sentryUserGroup->inGroup($testers) || $testerLoggedIn)
 				<div class="col3">
 				
 						@include('layouts.classBlock', array( 'rating' => isset($stars[$evercisegroup->id])?  array_sum($stars[$evercisegroup->id])/ count($stars[$evercisegroup->id]) : 0 ,  'evercisegroupId' => $evercisegroup->id,'title' => $evercisegroup->name , 'description' =>$evercisegroup->description ,  'image' => 'profiles/'.$evercisegroup->user->directory .'/'. $evercisegroup->image, 'category' => $evercisegroup->category->name , 'venue' => $evercisegroup->venue  , 'sessions' => $evercisegroup->futuresessions,  'default_price' => $evercisegroup->default_price, 'default_size' => $evercisegroup->capacity ))
