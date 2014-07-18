a7fd9c149a2571f93c2ced389251475eb0b2f6c4
tristanallen <furryfool@gmail.com>
2014-07-18 11:05:59 +0100

    stopped tester classes showing up in recommended

--- app/views/evercisegroups/recommended.blade.php
+++ app/views/evercisegroups/recommended.blade.php
@@ -1,33 +1,42 @@
 <div class="recommended-vertical-wrap">
 	<h5>Recommended Classes</h5>
 	@foreach( $evercisegroups as $key => $evercisegroup)
-		<div class="recommended-block">
-			<div class="block-header">
-				<a href="{{ URL::to('evercisegroups/'.$evercisegroup->id) }}">
-					{{ HTML::image('profiles/'.$evercisegroup->user->directory.'/'.$evercisegroup->user->image, 'class image', array('class' => 'recommended-block-img')); }}
-				</a> 
-				<h6>{{ Str::limit($evercisegroup->name, 14 )}}</h6>
-			</div>
-			
-			
+		<?php
+			 $sentryUserGroup = Sentry::findUserById($evercisegroup->user->id);
+			 $sentryUser = Sentry::getUser();
+			 $testers = Sentry::findGroupById(5);
 
-			<div class="recommended-info">
-				<div class="recommended-aside">
-				@include('ratings.stars', ['rating' => count($evercisegroup->ratings) == 0 ?  0 :  $ratings[$evercisegroup->id] / count($evercisegroup->ratings)])
+			 $testerLoggedIn = false;
+			 if($sentryUser) $testerLoggedIn = $sentryUser->inGroup($testers);
+
+		 ?>
+		 @if (!$sentryUserGroup->inGroup($testers) || $testerLoggedIn)
+			<div class="recommended-block">
+				<div class="block-header">
+					<a href="{{ URL::to('evercisegroups/'.$evercisegroup->id) }}">
+						{{ HTML::image('profiles/'.$evercisegroup->user->directory.'/'.$evercisegroup->user->image, 'class image', array('class' => 'recommended-block-img')); }}
+					</a> 
+					<h6>{{ Str::limit($evercisegroup->name, 14 )}}</h6>
+				</div>
 				
+				
+
+				<div class="recommended-info">
+					<div class="recommended-aside">
+					@include('ratings.stars', ['rating' => count($evercisegroup->ratings) == 0 ?  0 :  $ratings[$evercisegroup->id] / count($evercisegroup->ratings)])
+					
+					</div>
+					<div class="recommended-aside">
+						{{ HTML::image('img/category/'.$evercisegroup->category->name.'.png', 'category image', array('class' => 'category-icon')); }}
+						<span>{{ Str::limit($evercisegroup->category->name, 9) }}</span>
+					</div>
 				</div>
-				<div class="recommended-aside">
-					{{ HTML::image('img/category/'.$evercisegroup->category->name.'.png', 'category image', array('class' => 'category-icon')); }}
-					<span>{{ Str::limit($evercisegroup->category->name, 9) }}</span>
+				<div class=" block-footer">
+					<span>Price</span>
+					<strong>&pound;{{ $evercisegroup->default_price }}</strong>
 				</div>
 			</div>
-			<div class=" block-footer">
-				<span>Price</span>
-				<strong>&pound;{{ $evercisegroup->default_price }}</strong>
-			</div>
-		</div>
-		
-	
-@endforeach	
+		@endif
+	@endforeach	
 </div>
 
