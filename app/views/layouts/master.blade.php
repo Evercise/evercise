51678eb71a7c8d88275921e9606eff77d85b2320
tristanallen <furryfool@gmail.com>
2014-07-25 17:08:53 +0100

    installed mouseflow

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -29,6 +29,15 @@
 	<div class="mask">
 
 	</div>
+
+	<script type="text/javascript">
+	   var _mfq = _mfq || [];
+	   (function() {
+	       var mf = document.createElement("script"); mf.type = "text/javascript"; mf.async = true;
+	       mf.src = "//cdn.mouseflow.com/projects/c0202bf2-270c-42d5-9231-3d03589665ab.js";
+	       document.getElementsByTagName("head")[0].appendChild(mf);
+	   })();
+	</script>
 		
 </body>
 </html>
