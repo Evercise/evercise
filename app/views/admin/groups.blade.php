ede58dfd18f8b7123d50c24136bb823ae034608a
tristanallen <furryfool@gmail.com>
2014-07-30 17:29:39 +0100

    view evercisegroup subcats. started evercoin only pay

--- app/views/admin/groups.blade.php
+++ app/views/admin/groups.blade.php
@@ -10,10 +10,10 @@
 
         {{ Form::open(array('id' => 'venue_create', 'url' => 'venues', 'method' => 'post', 'class' => 'create-form')) }}
         <p>{{ $evercisegroup->name }}</p>
-
-        @include('widgets.autocomplete-category', ['fieldname'=>'category1', 'label'=>'category 1', 'force'=>1, 'selectedCategory'=>$evercisegroup->subcategory])
-        @include('widgets.autocomplete-category', ['fieldname'=>'category2', 'label'=>'category 2', 'force'=>1])
-        @include('widgets.autocomplete-category', ['fieldname'=>'category3', 'label'=>'category 3', 'force'=>1])
+        
+        @include('widgets.autocomplete-category', ['fieldname'=>'category1', 'label'=>'category 1', 'force'=>1, 'selectedCategory'=>count($evercisegroup->subcategories)>0 ? $evercisegroup->subcategories[0]->name : ''])
+        @include('widgets.autocomplete-category', ['fieldname'=>'category2', 'label'=>'category 2', 'force'=>1, 'selectedCategory'=>count($evercisegroup->subcategories)>1 ? $evercisegroup->subcategories[1]->name : ''])
+        @include('widgets.autocomplete-category', ['fieldname'=>'category3', 'label'=>'category 3', 'force'=>1, 'selectedCategory'=>count($evercisegroup->subcategories)>2 ? $evercisegroup->subcategories[2]->name : ''])
 
         {{ Form::close() }}
 		
