371e3ea3213abfcab312cbdd1e3f93dd6b1ee219
tristanallen <furryfool@gmail.com>
2014-07-31 17:33:31 +0100

    added 'amazon' database config

--- app/config/database.php
+++ app/config/database.php
@@ -212,3 +212,79 @@ else if (App::environment('production'))
 
 	);
 }
+else if (App::environment('amazon'))
+{
+	return array(
+
+		'fetch' => PDO::FETCH_CLASS,
+
+		'default' => 'mysql',
+
+		'connections' => array(
+
+			'sqlite' => array(
+				'driver'   => 'sqlite',
+				'database' => __DIR__.'/../database/production.sqlite',
+				'prefix'   => '',
+			),
+
+			'mysql' => array(
+				'driver'    => 'mysql',
+				'host'      => '127.0.0.1',
+				'database'  => 'evercise_2_0',
+				'username'  => '',
+				'password'  => '',
+				'charset'   => 'utf8',
+				'collation' => 'utf8_unicode_ci',
+				'prefix'    => '',
+			),
+			
+			'mysql_import' => array(
+				'driver'    => 'mysql',
+				'host'      => '127.0.0.1',
+				'database'  => 'EverciseDB_beta_1_4',
+				'username'  => '',
+				'password'  => '',
+				'charset'   => 'utf8',
+				'collation' => 'utf8_unicode_ci',
+				'prefix'    => '',
+			),
+
+			'pgsql' => array(
+				'driver'   => 'pgsql',
+				'host'     => 'localhost',
+				'database' => 'database',
+				'username' => 'root',
+				'password' => '',
+				'charset'  => 'utf8',
+				'prefix'   => '',
+				'schema'   => 'public',
+			),
+
+			'sqlsrv' => array(
+				'driver'   => 'sqlsrv',
+				'host'     => 'localhost',
+				'database' => 'database',
+				'username' => 'root',
+				'password' => '',
+				'prefix'   => '',
+			),
+
+		),
+
+		'migrations' => 'migrations',
+
+		'redis' => array(
+
+			'cluster' => false,
+
+			'default' => array(
+				'host'     => '127.0.0.1',
+				'port'     => 6379,
+				'database' => 0,
+			),
+
+		),
+
+	);
+}
