b4c35cb4faab9367953afae8d52328f44aa4dbce
tristanallen <furryfool@gmail.com>
2014-08-04 12:27:54 +0100

    sunjie setings. Localisations

---
+++ app/config/localisations/general.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+  'title' => 'Everyone exercise',
+  'meta_description' => 'Lower your barrier to enjoy fitness classes, Flexible schedule and multiple options across London.',
+  
+);
--- app/config/packages/codesleeve/asset-pipeline/config.php
+++ app/config/packages/codesleeve/asset-pipeline/config.php
@@ -207,7 +207,7 @@ return array(
 	| of minification stuff on each page refresh)
 	|
 	*/
-	'concat' => array('production', 'local', 'staging', 'amazonsandbox'),
+	'concat' => array('production', 'local', 'staging', 'amazonsandbox', 'sunjie'),
 
 	/*
 	|--------------------------------------------------------------------------
