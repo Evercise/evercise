96ecb28e98ec3cee431b7facef748d65b461bd85
Lewis Bayfield <bassymew@gmail.com>
2014-08-04 15:07:15 +0100

    trainer tc004

--- app/controllers/widgets/imageController.php
+++ app/controllers/widgets/imageController.php
@@ -56,10 +56,9 @@ class ImageController extends \BaseController {
             if(Request::ajax())
             { 
                 $viewString = View::make('widgets/crop')->__toString();
-               // $imgSrc = url() .'/'. $destinationPath . '/' . $filename;
-                $imgSrc = 'localhost/'. $destinationPath . '/' . $filename;
+                $imgSrc = url('/') .'/'. $destinationPath . '/' . $filename;
                // $postCrop = Response::json(route('image.crop.post'));
-                $postCrop = url().'/widgets/crop';
+                $postCrop = url('/').'/widgets/crop';
                 return Response::json(array('crop'=>$viewString, 'image_url' => $imgSrc , 'postCrop' => $postCrop));
             }
             else
