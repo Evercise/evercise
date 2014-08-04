9a56b7886f5baa03fbfca198089c4bd1ab86fb26
tristanallen <furryfool@gmail.com>
2014-08-04 11:23:16 +0100

    some changes to search functions + localisations

---
+++ app/config/localisations/home.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+        'main_header' => 'Lower your barrier to enjoy fitness classes',
+        'sub_header' => 'Flexible schedule and multiple options across London.',
+
+        'three_steps' => 'Get fit with Evercise in three simple steps:',
+        'step_1' => 'Search fitness classes',
+        'step_2' => 'Sign up to a class online',
+        'step_3' => 'Show up and shape up!',
+
+        'featured_classes' => 'Featured Classes',
+        'register_text' => 'Join the Evercise community today and find<br>your way to a happier, fitter you...',
+    );
