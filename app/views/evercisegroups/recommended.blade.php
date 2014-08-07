e539d49e4cfaa419c730e43d5c41cb7da1e5e154
Lewis Bayfield <bassymew@gmail.com>
2014-08-07 13:42:09 +0100

    redommended box

--- app/views/evercisegroups/recommended.blade.php
+++ app/views/evercisegroups/recommended.blade.php
@@ -9,20 +9,6 @@
 				<h6>{{ Str::limit($evercisegroup->name, 14 )}}</h6>
 			</div>
 			
-			
-
-			<div class="recommended-info">
-				<div id="block-share" class="recommended-aside">
-					<a href="{{ Share::load(URL::to('evercisegroups/'.$evercisegroup->id) , $evercisegroup->name)->facebook()  }}" target="_blank"  class="btn">{{ HTML::image('img/facebook.png','share on facebook', array('class' => 'share-icon')) }}</a>
-					<a href="{{ Share::load(URL::to('evercisegroups/'.$evercisegroup->id) , $evercisegroup->name)->twitter()  }}" target="_blank" class="btn">{{ HTML::image('img/twitter.png','tweet', array('class' => 'share-icon')) }}</a>
-
-				</div>
-				<div class="recommended-aside">
-				@include('ratings.stars', ['rating' => count($evercisegroup->ratings) == 0 ?  0 :  $ratings[$evercisegroup->id] / count($evercisegroup->ratings)])
-				
-				</div>
-
-			</div>
 			<div class=" block-footer">
 				<span>Price</span>
 				<strong>&pound;{{ $evercisegroup->default_price }}</strong>
