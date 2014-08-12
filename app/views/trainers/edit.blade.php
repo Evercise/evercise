915f1045b29120b63d3814c2bba7c197c4c888cb
Lewis Bayfield <bassymew@gmail.com>
2014-08-12 14:29:52 +0100

    cache control for logout

--- app/views/trainers/edit.blade.php
+++ app/views/trainers/edit.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.master')
 <?php
+/*
   //set headers to NOT cache a page
   header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
   header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+  */
 ?>
 
 @section('content')
