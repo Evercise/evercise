e732c9dfc9ac8fec8675ff6f81f838c5547e6ea8
Lewis Bayfield <bassymew@gmail.com>
2014-08-15 14:54:43 +0100

    version

--- app/lang/en/landing.php
+++ app/lang/en/landing.php
@@ -3,8 +3,8 @@
 /* ppc landing page content */
 
 return[
-	'header' => 'sign up today and get upto <span class="highlight">&pound;8*</span> in evercoins',
-	'terms' => '*Evercoins are not redeemable for cash and cannot be returned for a cash refund.',
+	'header' => 'sign up today and receive upto <span class="highlight">&pound;8*</span>',
+	'terms' => '*&pound;8 of credit in evercoins that are not redeemable for cash and cannot be returned for a cash refund.',
 	'bodyHeader' => 'What is evercise?',
 	'whatIsEvercise' => 'Evercise is an online platform designed to give people wanting to exercise, access to a wide range of fitness classes. We help those who wish for an easy going lifestyle to a regime that works around them due to date, type and location.',
 	'whyEvercise' => 'There are no signing up or membership fees, so you can simply sign up to a class of your choice on a class by class basis. It&apos;s based on your terms and rotates around your time. Fitness has just got a lot more fun, easier and flexible.',
