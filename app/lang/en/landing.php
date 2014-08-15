26002330b2a1ba8140a1b9940ea44078914c6693
Lewis Bayfield <bassymew@gmail.com>
2014-08-15 12:54:53 +0100

    landing

--- app/lang/en/landing.php
+++ app/lang/en/landing.php
@@ -5,5 +5,24 @@
 return[
 	'header' => 'sign up today and get upto <span class="highlight">&pound;8*</span> in evercoins',
 	'terms' => '*Evercoins are not redeemable for cash and cannot be returned for a cash refund.',
+	'bodyHeader' => 'What is evercise?',
+	'whatIsEvercise' => 'Evercise is an online platform designed to give people wanting to exercise, access to a wide range of fitness classes. We help those who wish for an easy going lifestyle to a regime that works around them due to date, type and location.',
+	'whyEvercise' => 'There are no signing up or membership fees, so you can simply sign up to a class of your choice on a class by class basis. It&apos;s based on your terms and rotates around your time. Fitness has just got a lot more fun, easier and flexible.',
+	'testimonialHeader' => 'Don&apos;t just take our word for it...',
+	'testimonials' => 
+	[
+		[
+			'name' => 'Gina',
+			'image' => 'Gina.png',
+			'location' => 'Islington, London',
+			'content' => 'Evercise has revolutionised the way I exercise. There is so much choice and I am saving a fortune. I find myself signing up for classes that I didn&apos;t even know existed!',
+		],
+		[
+			'name' => 'Sunita',
+			'image' => 'Sunita.png',
+			'location' => 'Southwark, London',
+			'content' => 'The flexibility that Eevercise offers is vital to my staying fit and healthy. The ability to workout when I have the time, with no financial commitment, really works for me.',
+		],
 
+	],
 ];
--- app/views/landings/create.blade.php
+++ app/views/landings/create.blade.php
@@ -5,9 +5,7 @@
 
 
 <div class="row">
-  <div class="styling-template">
-    {{ HTML::image('img/landing_page_concept_2_cropped.png') }}
-  </div>
+
   <header class="ppc">
     <section class="logo">
       {{ HTML::image('/img/evercise_logo.png', 'evercise logo', array('class' => 'logo')); }}
@@ -21,12 +19,50 @@
     {{ Form::open(array('id' => 'send_ppc', 'url' => 'landings', 'method' => 'POST', 'class' => 'create-form milestone-form')) }}
             {{ Form::label( 'email', 'Email Address') }}
             {{ Form::hidden( 'category' , $category->id , array('id' => 'category')) }}
-            {{ Form::text( 'email' , '', array('id' => 'email','placeholder' => 'Enter your email address')) }}
-            {{ Form::submit('Get Code' , array('class'=>'btn btn-yellow ')) }}
+            {{ Form::text( 'email' , '', array('id' => 'email','placeholder' => 'example@domain.com')) }}
+            {{ Form::submit('JOIN NOW' , array('class'=>'btn btn-black ')) }}
           {{ Form::close() }}
-          {{ HTML::link('ppc_fb/'.$category->id, 'Sign up with facebook', array('class' => 'btn btn-fb btn-large')) }}
+
+          <a href="URL()">
+
+          {{ HTML::decode(HTML::link('ppc_fb/'.$category->id, '<img src="'.url().'/img/facebook_logo.png" /> Log in with Facebook', ['alt'=>"facebook icon", 'class' => 'btn btn-fb'] ) )}}
   </div>
+  <div class="landing-body">
+    <h2>{{trans('landing.bodyHeader')}}</h2>
+    <aside>
+      <p>{{trans('landing.whatIsEvercise')}}</p>
+    </aside>
+    <aside>
+      <p>{{trans('landing.whyEvercise')}}</p>
+    </aside>
+  </div>
+
+  <div class="landing-body grey">
+    <h2>{{trans('landing.testimonialHeader')}}</h2>
+     @foreach(trans('landing.testimonials') as $testimonial)
+      <aside>
+        <div class="testimonial-block">
+          {{ HTML::image('img/'.$testimonial['image']) }}
+          <strong>{{ $testimonial['name']}}</strong>
+          <br>
+          <i>{{ $testimonial['location']}}</i>
+          <div class="content">
+            <p>{{ $testimonial['content']}}</p>
+          </div>
+          
+        </div>
+      </aside>
+    @endforeach
+  </div>
+
+<div class="footer-block">
+  {{ HTML::linkRoute('static.terms_of_use', 'Terms of Use') }}
+  {{ HTML::linkRoute('static.privacy', trans('footer.privacy_policy') ) }}
+  <span>{{trans('footer.copyright')}}</span>
 </div>
+ 
+</div>
+
 {{--
     <div id="about" class="center_col">
       <p>PPC</p>
