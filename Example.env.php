b88f9408e12601dd10216709389dc29e17b3e53b
Igor Matkovic <igor@cod3.me>
2014-08-18 11:58:13 +0100

    Add Example Environment File

---
+++ Example.env.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This is a Example Enviroment file
+ *
+ * Make a copy of this file and name it .env.php
+ *
+ * After your copied it just edit the contents of it to match your setup
+ */
+return [
+
+    'DEBUG_APP'      => true,
+    'APP_URL'        => 'http://evercise.dev/',
+    'ENCRYPTION_KEY' => 'ozt38MwirMfb5STSJWowmnHBGUz0ziAR',
+
+    'DB_HOST'        => 'localhost',
+    'DB_NAME'        => 'evercise',
+    'DB_USER'        => 'evercise',
+    'DB_PASS'        => 'evercise',
+
+    'DB_V1_HOST'     => 'localhost',
+    'DB_V1_NAME'     => 'evercise',
+    'DB_V1_USER'     => 'evercise',
+    'DB_V1_PASS'     => 'evercise',
+
+
+];
