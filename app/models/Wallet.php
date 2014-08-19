c32d329fa0bcbc77cc1834c7a81e6e9f62e7beba
Igor Matkovic <igor@cod3.me>
2014-08-19 10:37:21 +0100

    Update To wallet controller and model

--- app/models/Wallet.php
+++ app/models/Wallet.php
@@ -1,57 +1,68 @@
 <?php
 
-class Wallet extends \Eloquent {
+class Wallet extends \Eloquent
+{
 
-	protected $fillable = ['id', 'user_id', 'balance', 'previous_balance'];
+    protected $fillable = ['id', 'user_id', 'balance', 'previous_balance'];
 
-	protected $table = 'wallets';
+    protected $table = 'wallets';
 
-    public function deposit( $amount, $sessionpayment_id=0 )
+    public function deposit($amount, $sessionpayment_id = 0)
     {
-    	$this->transaction($amount, $sessionpayment_id);
+        $this->transaction($amount, $sessionpayment_id);
     }
 
-    public function withdraw( $amount, $sessionpayment_id=0 )
+    public function withdraw($amount, $sessionpayment_id = 0)
     {
-    	$this->transaction(-$amount, $sessionpayment_id);
+        $this->transaction(- $amount, $sessionpayment_id);
     }
 
     public function recordedSave(array $params)
     {
-    	//$transaction_amount = $params['balance'] - $params['previous_balance'];
-    	
-		Wallethistory::create([
-			'user_id'=>$params['user_id'],
-			'transaction_amount'=>$params['transaction_amount'],
-			'new_balance' => $params['new_balance'],
-			'sessionpayment_id' => $params['sessionpayment_id']
-		]);
+        //$transaction_amount = $params['balance'] - $params['previous_balance'];
 
-   		parent::save();
+        Wallethistory::create(
+            [
+                'user_id'            => $params['user_id'],
+                'transaction_amount' => $params['transaction_amount'],
+                'new_balance'        => $params['new_balance'],
+                'sessionpayment_id'  => $params['sessionpayment_id']
+            ]
+        );
+
+        parent::save();
 
     }
-    protected function transaction( $amount, $sessionpayment_id )
+
+    protected function transaction($amount, $sessionpayment_id)
     {
-    	
-    	$user_id = $this->attributes['user_id'];
-    	$oldBalance = $this->attributes['balance'];
-    	$newBalance = $oldBalance + $amount;
-    	$this->attributes['balance'] = $newBalance;
 
-    	$this->save();
+        $user_id = $this->attributes['user_id'];
+        $this->attributes['balance'] = $this->attributes['balance'] + $amount;
 
-		Wallethistory::create([
-			'user_id'=>$user_id,
-			'sessionpayment_id' => $sessionpayment_id,
-			'transaction_amount'=>$amount,
-			'new_balance' => $newBalance
-		]);
-	}
-	public function updatePaypal($newPaypal)
-	{
-		$this->attributes['paypal'] = $newPaypal;
-    	$this->save();
-	}
+        $this->save();
+
+        Wallethistory::create(
+            [
+                'user_id'            => $user_id,
+                'sessionpayment_id'  => $sessionpayment_id,
+                'transaction_amount' => $amount,
+                'new_balance'        => $this->attributes['balance']
+            ]
+        );
+    }
+
+    public function updatePaypal($newPaypal)
+    {
+        $this->attributes['paypal'] = $newPaypal;
+        $this->save();
+    }
+
+
+    public static function userWallet($user_id)
+    {
+        return Wallet::where('user_id', $user_id)->first();
+    }
 
 }
   
