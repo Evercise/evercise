41f260f71c3b142c1523923a18566d78d18ca2e9
Igor Matkovic <igor@cod3.me>
2014-08-22 16:23:08 +0100

    Some defaults for CI

---
+++ app/config/testing/database.php
@@ -0,0 +1,76 @@
+<?php
+
+
+return array(
+
+    'fetch' => PDO::FETCH_CLASS,
+
+    'default' => 'mysql',
+
+    'connections' => array(
+
+        'sqlite' => array(
+            'driver'   => 'sqlite',
+            'database' => __DIR__.'/../database/production.sqlite',
+            'prefix'   => '',
+        ),
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => (getenv('DB_HOST') ?: '127.0.0.1'),
+            'database'  => (getenv('DB_NAME') ?: 'evertest'),
+            'username'  => (getenv('DB_USER') ?: 'shippable',
+            'password'  => (getenv('DB_PASS') ?: ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ),
+
+        'mysql_import' => array(
+            'driver'    => 'mysql',
+            'host'      => (getenv('DB_V1_HOST') ?: '127.0.0.1'),
+            'database'  => getenv('DB_V1_NAME'),
+            'username'  => getenv('DB_V1_USER'),
+            'password'  => getenv('DB_V1_PASS'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ),
+
+        'pgsql' => array(
+            'driver'   => 'pgsql',
+            'host'     => 'localhost',
+            'database' => 'database',
+            'username' => 'root',
+            'password' => '',
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+        ),
+
+        'sqlsrv' => array(
+            'driver'   => 'sqlsrv',
+            'host'     => 'localhost',
+            'database' => 'database',
+            'username' => 'root',
+            'password' => '',
+            'prefix'   => '',
+        ),
+
+    ),
+
+    'migrations' => 'migrations',
+
+    'redis' => array(
+
+        'cluster' => false,
+
+        'default' => array(
+            'host'     => '127.0.0.1',
+            'port'     => 6379,
+            'database' => 0,
+        ),
+
+    )
+
+);
