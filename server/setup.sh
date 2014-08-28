bc59c3c4d37bf3d775e70e76938cd187568d0e6d
Igor Matkovic <igor@cod3.me>
2014-08-28 14:18:12 +0100

    Fix for Sql Server (partly)

--- server/setup.sh
+++ server/setup.sh
@@ -1,10 +1,21 @@
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+
+
+echo "Moving Config Files"
+sudo \cp -r /var/www/html/server/my.cnf /etc/my.cnf
+sudo \cp -r /var/www/html/server/php.ini /etc/php.ini
+
+
+sudo service httpd restart
+sudo service mysqld restart
+
+
 echo "START MAILCATCHER"
 mailcatcher --ip=0.0.0.0
 
 echo "Seting PERMISSIONS"
 cd /var/www/html
-
-chmod 777 -R app/storage/*
 
 php artisan clear-compiled
 
