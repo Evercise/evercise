765b3c5d64c5d9ec5c62f284f3d090cf7d05b05a
Lewis Bayfield <bassymew@gmail.com>
2014-08-28 13:46:50 +0100

    css n js version

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -14,8 +14,8 @@
 	<link href='http://fonts.googleapis.com/css?family=Exo:300,400,500,600,300italic,400italic,500italic,600italic' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
 
-	<link href="/assets/application.css?version=2.7" rel="stylesheet" type="text/css">
-	<script src="/assets/application.js?version=2.7"></script>
+	<link href="/assets/application.css?version=2.8" rel="stylesheet" type="text/css">
+	<script src="/assets/application.js?version=2.8"></script>
 
 </head>
 <body>
