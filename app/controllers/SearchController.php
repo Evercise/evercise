209d79c69b854678866e3048c22e2ad6817763c5
Igor Matkovic <igor@Igors-MacBook-Pro-2.local>
2014-09-11 13:40:26 +0100

    Make Controller More Dependancy Injectable

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -4,6 +4,38 @@
 class SearchController extends \BaseController
 {
 
+    protected $evercisegroup;
+    protected $sentry;
+    protected $link;
+    protected $input;
+    protected $log;
+    protected $place;
+    protected $elastic;
+    protected $search;
+
+    public function __construct(
+        Evercisegroup $evercisegroup,
+        Sentry $sentry,
+        Link $link,
+        Illuminate\Http\Request $input,
+        Illuminate\Log\Writer $log,
+        Es $elasticsearch,
+        Geotools $geotools,
+        Place $place
+    ) {
+
+        $this->evercisegroup = $evercisegroup;
+        $this->sentry = $sentry;
+        $this->link = $link;
+        $this->input = $input;
+        $this->log = $log;
+        $this->place = $place;
+
+        $this->elastic = new Elastic($geotools::getFacadeRoot(), $this->evercisegroup, $elasticsearch::getFacadeRoot(), $this->log);
+        $this->search = new Search($this->elastic, $this->evercisegroup, $this->log);
+
+    }
+
     /**
      * Display the specified resource.
      *
@@ -12,12 +44,12 @@ class SearchController extends \BaseController
      */
     public function show($id)
     {
-        if ($evercisegroup = Evercisegroup::with('Evercisesession.Sessionmembers.Users')
+        if ($evercisegroup = $this->evercisegroup->with('Evercisesession.Sessionmembers.Users')
             ->with('evercisesession.sessionpayment')
             ->with('subcategories.categories')
             ->find($id)
         ) {
-            if (Sentry::check() && $evercisegroup->user_id == $this->user->id
+            if ($sentry->check() && $evercisegroup->user_id == $this->user->id
             ) // This Group belongs to this User/Trainer
             {
                 return $evercisegroup->showAsOwner($this->user);
@@ -37,7 +69,7 @@ class SearchController extends \BaseController
      */
     public function parseUrl($all_segments = '')
     {
-        $link = Link::checkLink($all_segments);
+        $link = $this->link->checkLink($all_segments);
 
 
         if ($link) {
@@ -66,21 +98,21 @@ class SearchController extends \BaseController
     public function search($area = false)
     {
 
-        $input = Input::all();
+        $input = $this->input->all();
 
         /** If Area is not a object lets add it to the database so we have it for later use  */
         if (!$area instanceof Place) {
 
-            Log::info('NoT Place');
+            $this->log->info('NoT Place');
             if (!isset($input['location'])) {
                 $input['location'] = 'London';
             }
-            $location = Place::getByLocation($input['location']);
+            $location = $this->place->getByLocation($input['location']);
 
             unset($input['location']);
-            //We have save the location to the DB so we can redirect the user to the new URL now
+            /** We have save the location to the DB so we can redirect the user to the new URL now */
 
-            Log::info('Redirect TO: ' . $location->link->permalink . '?' . http_build_query($input));
+            $this->log->info('Redirect TO: ' . $location->link->permalink . '?' . http_build_query($input));
             $input['allsegments'] = $location->link->permalink;
 
             return Redirect::route(
@@ -92,13 +124,53 @@ class SearchController extends \BaseController
 
         }
         $params = [
-            'page'     => Input::get('page', 1),
-            'radius'   => Input::get('radius', 1),
-            'category' => Input::get('category')
+            'page'   => $this->input->get('page', 1),
+            'radius' => $this->input->get('radius', '1mi'),
+            'search' => $this->input->get('search')
         ];
 
 
-        return Search::newSearch($area, $params, $this->user);
+        $searchResults = $this->search->getResults($area, $params);
+
+
+        echo "<pre>";
+        print_r($searchResults);
+        die();
+
+        $perPage = 12;
+
+
+        if ($page > count($allResults) or $page < 1) {
+            $page = 1;
+        }
+        $offset = ($page * $perPage) - $perPage;
+        $articles = array_slice($allResults, $offset, $perPage);
+        $paginatedResults = Paginator::make($articles, count($allResults), $perPage);
+
+        foreach ($allResults as $result) {
+            unset($result['description']);
+            unset($result['default_duration']);
+            unset($result['published']);
+            unset($result['created_at']);
+            unset($result['updated_at']);
+            unset($result['user']);
+            unset($result['venue_id']);
+            unset($result['title']);
+            unset($result['gender']);
+            $mapResult[] = $result->toJson();
+        }
+
+        //return json_encode($mapResult);
+
+
+        return View::make('evercisegroups.search')
+            ->with('places', json_encode($mapResult))
+            ->with('evercisegroups', $paginatedResults);
+
+        echo "<pre>";
+        print_r($res);
+        die();
+        dd($res);
     }
 
 
