75b385a49bcdae73a24872a951ac88db684a212f
Igor Matkovic <igor@Igors-MacBook-Pro-2.local>
2014-09-15 11:58:13 +0100

    Adjust Views

--- app/views/widgets/autocomplete-location.blade.php
+++ app/views/widgets/autocomplete-location.blade.php
@@ -3,7 +3,15 @@
 <script type="text/javascript" src="//maps.googleapis.com/maps/api/js?libraries=places"></script>
 @endif
 
+
 <div id="locationField">
-  <input id="location" placeholder="{{trans('discover.location_box')}}" type="text" name="location" value="{{ isset($selectedLocation) ? $selectedLocation : '' }}"/>
+  <script type="text/javascript">
+        var area_id = '<?=(!empty($area->name) ? $area->name : '') ?>';
+        var area_name = '<?=(!empty($area->name) ? $area->name : '') ?>';
+        var area_link = '<?=(!empty($area->link->permalink) ? $area->link->permalink : '')?>';
+  </script>
+  <input type="hidden" name="area_id"  id="area_id" value="<?=(!empty($area->id) ? $area->id : '') ?>"/>
+  <input id="location"
+  placeholder="{{trans('discover.location_box')}}" type="text" name="location" value="<?=(!empty($area->name) ? $area->name : '') ?>"/>
 </div>
  
