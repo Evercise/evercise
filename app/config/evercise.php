fedbba41842c15709c5d7d58482c8cbbd0c3580b
Igor Matkovic <igor@Igors-MacBook-Pro-2.local>
2014-09-15 11:58:02 +0100

    Global Evercise config

---
+++ app/config/evercise.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+return [
+    'per_page'         => [12, 18, 24],
+    'default_per_page' => 12,
+    'radius'           => [
+        '1/2 mile' => '0.5mi',
+        '1 mile'   => '1mi',
+        '2 miles'  => '2mi',
+        '3 miles'  => '3mi',
+        '5 miles'  => '5mi'
+    ],
+    'default_radius'   => '0.5mi'
+];
