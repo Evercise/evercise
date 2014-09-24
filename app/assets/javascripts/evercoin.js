a6be951eabe7148ea45dfe5f1d2fa06d5192761f
Lewis Bayfield <bassymew@gmail.com>
2014-09-24 12:17:59 +0100

    remove key up for keypress

--- app/assets/javascripts/evercoin.js
+++ app/assets/javascripts/evercoin.js
@@ -40,12 +40,6 @@ function initRedeemEvercoin(params)
                 $('#redeem').val(evercoinBalance);
             }
         })
-
-        $('#p_first').keypress(function(event){
-            console.log(event.which);
-            if(event.which != 8 && isNaN(String.fromCharCode(event.which))){
-                event.preventDefault();
-            }});
 	};
 
 
