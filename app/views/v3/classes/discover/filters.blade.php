657ceb4c9931ddd4579c1c1d924be74a4552e081
Lewis Bayfield <bassymew@gmail.com>
2014-11-20 17:02:54 +0000

    serach dropdown

--- app/views/v3/classes/discover/filters.blade.php
+++ app/views/v3/classes/discover/filters.blade.php
@@ -1,7 +1,7 @@
 <div class="discover-nav container-fluid">
     <div class="container">
         <div class="row">
-            {{ Form::open(['route' => 'home', 'method' => 'post', 'id' => 'search-classes']) }}
+            {{ Form::open(['route' => 'evercisegroups.search', 'method' => 'get', 'id' => 'search-classes']) }}
                 <div class="col-sm-4">
                     <div class="input-group with-addon">
                         <div class="input-group-addon"><span class="icon icon-search"></span></div>
@@ -15,6 +15,7 @@
                     </div>
                 </div>
                 <div class="col-sm-4">
+                    {{ Form::hidden('distance', '2mi') }}
                     {{ Form::submit('Find a Class', ['class' => 'btn btn-primary mr10']) }}
                     <div class="pull-right mt10">
                         <span ng-click="changeView('gridview')" class="icon icon-grid mr10 hover" ng-class="{active : view == 'gridview'}"></span>
@@ -23,48 +24,9 @@
                     </div>
                 </div>
 
-
-
-                <!--<div class="col-sm-5">
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <div class="input-group col-sm-12 with-addon">
-                                <div class="input-group-addon"><span class="icon icon-search"></span></div>
-                                <input class="form-control" type="text" placeholder="Search for Classes...">
-                                <span class="input-group-btn">
-                                    <button class="btn btn-primary" type="button">
-                                         Find a Class
-                                    </button>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="col-sm-7">
-                    <div class="row">
-
-                        <div class="col-sm-4 no-gutter">
-                            <div class="form-group">
-                                <span ng-click="changeView('gridview')" class="icon icon-grid mr10 hover" ng-class="{active : view == 'gridview'}"></span>
-                                <span ng-click="changeView('listview')" class="icon icon-list mr10 hover" ng-class="{active : view == 'listview'}"></span>
-                                <span ng-click="changeView('mapview')" class="icon icon-map hover" ng-class="{active : view == 'mapview'}"></span>
-                            </div>
-
-                        </div>
-                    </div>
-
-                </div>-->
-
-
             {{ Form::close() }}
         </div>
 
 
     </div>
-
-
-
-
-
 </div>
