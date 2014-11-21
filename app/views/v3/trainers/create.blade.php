537bfd42549ad4c0200e228b92156f4d38ec71ab
Tristan <tristangallen@gmail.com>
2014-11-21 10:03:29 +0000

    User and Trainer signup

--- app/views/v3/trainers/create.blade.php
+++ app/views/v3/trainers/create.blade.php
@@ -11,21 +11,22 @@
                 <div class="text-center mb50">
                     <strong>Your account has been created, just a few more details and then you can create classes.</strong>
                 </div>
-                {{ Form::open(['url' => '', 'method' => 'post', 'class'=>'mb50', 'role' => 'form', 'id' => 'register-form'] ) }}
+                {{ Form::open(['url' => 'ajax/trainers/store', 'method' => 'post', 'class'=>'mb50', 'role' => 'form', 'id' => 'register-form'] ) }}
                    <div class="form-group mb50">
                         @include('v3.widgets.profile_image_upload')
+                        {{ Form::hidden('image', 'fucksticks', ['id' => 'image']) }}
                    </div>
                    <div class="form-group mb50">
                         {{ Form::label('profession', 'Add your Profession', ['class' => 'mb15'] )  }}
                         {{ Form::text('profession', null, ['class' => 'form-control', 'placeholder' => 'Max 50 Characters', 'maxlength' => 50]) }}
                    </div>
                    <div class="form-group mb50">
-                        {{ Form::label('profession', 'Add your Profession', ['class' => 'mb15'] )  }}
-                        {{ Form::textarea('profession', null, ['class' => 'form-control', 'placeholder' => 'Between 50 and 500 characters', 'maxlength' => 500, 'rows'=> 7]) }}
+                        {{ Form::label('profession', 'Add your Bio', ['class' => 'mb15'] )  }}
+                        {{ Form::textarea('bio', null, ['class' => 'form-control', 'placeholder' => 'Between 50 and 500 characters', 'maxlength' => 500, 'rows'=> 7]) }}
                    </div>
                    <div class="form-group mb50">
 
-                    <label class="mb15" for="phone">Phone Number<small> (Get alerts about classes)</small></label>
+                    <label class="mb15" for="phone">Add your website</label>
 
                     <div class="input-group">
                         <div class="input-group-addon">
