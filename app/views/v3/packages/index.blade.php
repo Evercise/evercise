1365e274bdc27972daccf51a5574370e5adc923c
Igor Matkovic <igor@Igors-MacBook-Pro-2.local>
2014-12-05 11:48:36 +0000

    Update Packages

--- app/views/v3/packages/index.blade.php
+++ app/views/v3/packages/index.blade.php
@@ -12,7 +12,7 @@
     <div class="row">
         @foreach($packages as $index => $p)
             <div class="col-sm-4 text-center">
-                <ul class="list-group package-block-module {{ $index == 0 ? 'package-green' : ($index == 1 ? 'package-blue' : 'package-pink') }}">
+                <ul class="list-group package-block-module 'package-{{ $p->style }}">
                     <li class="list-group-item package-title">
                         <h3>{{ $p->name }}</h3>
                     </li>
@@ -24,6 +24,9 @@
                         @if(!empty($p->bullets))
                             <div>
                                 <ul>
+                                    <li><span class="icon icon-point"></span>Any {{$p->classes }} classes up to £{{ round($p->max_class_price,2) }}</li>
+                                    <li><span class="icon icon-point"></span>Save {{$p->savings()}}% on standard price</li>
+                                    <li><span class="icon icon-point"></span>Up to {{ $p->availableClasses()}} classes to choose from</li>
                                     @foreach(explode('|', $p->bullets) as $b)
                                         <li><span class="icon icon-point"></span>{{ $b }}</li>
                                     @endforeach
@@ -34,8 +37,8 @@
                     <li class="list-group-item package-pay">
                         <div class="row">
                             <div class="col-sm-5 text-left">
-                                <strong>£{{$p->price}}</strong>
-                                <small>Savings of {{$p->savings}}%</small>
+                                <strong>£{{round($p->price,2)}}</strong>
+                                <small>Savings of {{$p->savings()}}%</small>
                             </div>
                             <div class="col-sm-7">
                                 {{ Form::open(['route'=> 'cart.add','method' => 'post', 'id' => 'add-to-class'. $p->id, 'class' => 'add-to-class']) }}
