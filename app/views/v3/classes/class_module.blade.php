5e2038c1a22531b10086b40093c6f1e054b5754b
Tristan <tristangallen@gmail.com>
2014-12-05 15:37:33 +0000

    class links on homepage

--- app/views/v3/classes/class_module.blade.php
+++ app/views/v3/classes/class_module.blade.php
@@ -1,7 +1,7 @@
 <!--
 <div class="class-module center-block">
     <div class="class-image-wrapper">
-         {{ HTML::decode( HTML::linkRoute('evercisegroups.show', image($class->user->directory. '/thumb_'. $class->image,  $class->name . ' cover image') , $class->id)  )}}
+         {{ HTML::decode( HTML::linkRoute('class.show', image($class->user->directory. '/thumb_'. $class->image,  $class->name . ' cover image') , $class->slug)  )}}
     </div>
     <div class="class-title-wrapper text-center">
         <a href="#"><h3>{{ $class->name }}</h3></a>
@@ -29,12 +29,12 @@
 
 <ul class="list-group class-module">
     <div class="class-image-wrapper">
-         {{ HTML::decode( HTML::linkRoute('evercisegroups.show', image($class->user->directory. '/module_'. $class->image,  $class->name . ' cover image', ['class' => 'img-responsive img-full-width']) , $class->id)  )}}
+         {{ HTML::decode( HTML::linkRoute('class.show', image($class->user->directory. '/module_'. $class->image,  $class->name . ' cover image', ['class' => 'img-responsive img-full-width']) , $class->slug)  )}}
     </div>
     <li class="list-group-item">
 
         <div class="class-title-wrapper text-center">
-            <a href="{{ URL::route('class.show', [$class->id]) }}"><h3>{{ $class->name }}</h3></a>
+            <a href="{{ URL::route('class.show', [$class->slug]) }}"><h3>{{ $class->name }}</h3></a>
             <div class="class-rating-wrapper">
                 @if (isset($class->ratings))
                     @include('v3.classes.ratings.stars', array('rating' => $class->ratings))
@@ -56,7 +56,7 @@
     <li class="list-group-item bg-light-grey">
         <div class="class-info-wrapper  row">
             <div class="col-xs-6" ><strong class="text-primary">&pound;{{ $class->futuresessions[0]->price }}</strong></div>
-            <div class="col-xs-6"> {{ HTML::linkRoute('evercisegroups.show', 'Join Class', $class->id, ['class'=>'btn btn-default pull-right']) }}</div>
+            <div class="col-xs-6"> {{ HTML::linkRoute('class.show', 'Join Class', $class->slug, ['class'=>'btn btn-default pull-right']) }}</div>
         </div>
     </li>
 </ul>
