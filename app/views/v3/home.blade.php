ab370ed66a6fa45628d6299ac2c7d13d2e7858e4
Lewis Bayfield <bassymew@gmail.com>
2014-12-08 13:50:56 +0000

    home assets img tags

--- app/views/v3/home.blade.php
+++ app/views/v3/home.blade.php
@@ -101,7 +101,7 @@
                         <h2>What is Evercise</h2>
                     </div>
 
-                    {{ image('img/home/what-is-evercise.png', 'what is evercise', ['class' => 'img-responsive center-block']) }}
+                    {{ image('img/home/wie.png', 'what is evercise', ['class' => 'img-responsive center-block']) }}
                     <div class="caption">
                         <p>Evercise is the exciting new Pay As You Go fitness community that’s flexible enough to fit in with your modern lifestyle. Evercise unites talented trainers with an active community who want more fun and freedom from their fitness routine. </p>
                     </div>
@@ -114,7 +114,7 @@
                         <h2>Why join Evercise</h2>
                     </div>
 
-                    {{ image('img/home/why-join-evercise.png', 'why join evercise', ['class' => 'img-responsive center-block']) }}
+                    {{ image('img/home/wje.png', 'why join evercise', ['class' => 'img-responsive center-block']) }}
                     <div class="caption">
                          <p>We want fitness to be fun and flexible rather than routine and restricted. By bringing together trainers and a keen community of fitness enthusiasts Evercise really does benefit everyone. Evercise makes fitness fun again, emphasising social, group exercise that fits in with your life</p>
                     </div>
@@ -127,7 +127,7 @@
                         <h2>How it works</h2>
                     </div>
 
-                    {{ image('img/home/how-it-works.png', 'how it works', ['class' => 'img-responsive center-block']) }}
+                    {{ image('img/home/hiw.png', 'how it works', ['class' => 'img-responsive center-block']) }}
                     <div class="caption">
                          <p>Whether you’re a trainer or a participant Evcercise is all about convenience. Once you’ve created an Evercise profile our smart platform helps trainers to find participants and participants to find their perfect class. All your booking and scheduling is done right here on Evercise.</p>
                     </div>
