5e286edccadf50b3bea4b8d74d805d13d00d7c9d
Lewis Bayfield <bassymew@gmail.com>
2014-12-09 13:38:48 +0000

    rtem,plate tweak

--- app/views/v3/emails/user/welcome.blade.php
+++ app/views/v3/emails/user/welcome.blade.php
@@ -6,42 +6,49 @@
 <p>The Evercise network gives you access to a huge array of fun and flexible fitness classes wherever you are and our simple three-step process means it&apos;s quick and easy to <span class="pink-text">sign up</span> and get involved.</p>
 @stop
 @section('extra')
-    <table width="640" height="auto" align="center" cellspacing="0" cellpadding="0" bgcolor="#ff1b7e">
-        <tr>
-            <td>
-                <div class="container">
-                    <div class="mb30">
-                        <div class="image-left">
-                            {{ image('/img/home/wie.png') }}
-                        </div>
-                        <br>
-                        <h3 class="white-text">Search</h3>
-                        <p class="white-text">Evercise makes it easy to search your area and discover the perfect class for you. With classes covering everything from aerobics to zumba you&apos;re sure to find something nearby that takes your fancy.</p>
-                    </div>
-                    <div class="mb30 text-right">
-                        <div class="image-right">
-                            {{ image('/img/home/wie.png') }}
-                        </div>
-                        <br>
-                        <h3 class="white-text">Select</h3>
-                        <p class="white-text">You can see reviews of all our classes, find out more about the venue and facilities and ask the trainer any questions you might have. </p>
-                    </div>
-                    <div class="mb30">
-                        <div class="image-left">
-                            {{ image('/img/home/wie.png') }}
-                        </div>
-                        <br>
-                        <h3 class="white-text">Sign up</h3>
-                        <p class="white-text">When you&apos;ve found a class you like the look of our simple Pay As You Go booking system ensures that joining your class is as quick and easy as ordering a pizza! </p>
-                    </div>
-                    <br>
-                    <br>
-                    <div class="text-center mb30">
-                        {{ Html::linkRoute('evercisegroups.search', 'Get Started', null, ['class' => 'btn btn-blue']) }}
-                    </div>
-
+    <table width="100%" height="auto" align="center" cellspacing="0" cellpadding="30" bgcolor="#ff1b7e">
+        <tr width="100%">
+            <td width="33%">
+                <div class="mb30">
+                    {{ image('/img/home/wie.png') }}
                 </div>
-
+            </td>
+            <td width="67%">
+                <h3 class="white-text">Search</h3>
+                <p class="white-text">Evercise makes it easy to search your area and discover the perfect class for you. With classes covering everything from aerobics to zumba you&apos;re sure to find something nearby that takes your fancy.</p>
+            </td>
+        </tr>
+    </table>
+    <table width="100%" height="auto" align="center" cellspacing="0" cellpadding="30" bgcolor="#ff1b7e">
+        <tr width="100%">
+            <td width="67%">
+                <h3 class="white-text">Select</h3>
+                <p class="white-text">You can see reviews of all our classes, find out more about the venue and facilities and ask the trainer any questions you might have. </p>
+            </td>
+            <td width="33%">
+                {{ image('/img/home/wie.png') }}
+            </td>
+        </tr>
+    </table>
+    <table width="100%" height="auto" align="center" cellspacing="0" cellpadding="30" bgcolor="#ff1b7e">
+        <tr width="100%">
+            <td width="33%">
+                {{ image('/img/home/wie.png') }}
+            </td>
+            <td width="67%">
+                <h3 class="white-text">Sign up</h3>
+                <p class="white-text">When you&apos;ve found a class you like the look of our simple Pay As You Go booking system ensures that joining your class is as quick and easy as ordering a pizza! </p>
+            </td>
+        </tr>
+    </table>
+    <table width="100%" height="auto" align="center" cellspacing="0" cellpadding="30" bgcolor="#ff1b7e">
+        <tr width="100%">
+            <td width="100%">
+                <br>
+                <br>
+                <div class="text-center mb30">
+                    {{ Html::linkRoute('evercisegroups.search', 'Get Started', null, ['class' => 'btn btn-blue']) }}
+                </div>
             </td>
         </tr>
     </table>
