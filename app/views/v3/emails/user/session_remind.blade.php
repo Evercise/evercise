f2f5d81386f1fcf1846735d2bab02f0e6eca4a6c
Tristan <tristangallen@gmail.com>
2014-12-10 16:23:09 +0000

    user class reminder

--- app/views/v3/emails/user/session_remind.blade.php
+++ app/views/v3/emails/user/session_remind.blade.php
@@ -4,27 +4,21 @@
 @section('body')
 
 
-<p>Hey _____</p>
+<p>Hey {{$name}}</p>
+<p>Don&apos;t forget your upcoming class!</p>
 
-<p>Don’t forget your upcoming class!</p>
+<p>{{ $group }}</p>
+<p>{{ $dateTime }}</p>
+<p>{{ $location }}</p>
+<p>{{ $trainerName }}</p>
+<p>{{ $trainerEmail }}</p>
 
-[CLASS DETAILS]
-
-Take note of your unique booking code. Your trainer will need this - along with
-
-another form of ID - to identify you.
-
-Please try to arrive 15 minutes before the specified start time. Late arrivals
-
-can be rejected.
-
-Remember to bring a water bottle, a towel or two and appropriate clothing.
-
-If you have any questions, feel free to contact the instructor here.
-
-Have fun!
+<p>Take note of your unique booking code. Your trainer will need this - along with another form of ID - to identify you.</p>
+<p>Please try to arrive 15 minutes before the specified start time. Late arrivals can be rejected.</p>
+<p>Remember to bring a water bottle, a towel or two and appropriate clothing.</p>
+<p>If you have any questions, feel free to contact the instructor here.</p>
+<p>Have fun!</p>
+<p>Fitness is even more fun with friends! Click on the link below to share this class.</p>
 
 
 @stop
-
-
