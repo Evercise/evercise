318cf3ee4721692806e9ead7811856a5807adec7
Lewis Bayfield <bassymew@gmail.com>
2014-12-12 14:23:08 +0000

    user reminder

--- app/views/v3/emails/user/session_remind.blade.php
+++ app/views/v3/emails/user/session_remind.blade.php
@@ -4,15 +4,11 @@
 @section('body')
 
 
-<p>Hey {{$name}}</p>
+<p>Hey <span class="blue-text">{{$name}}</span></p>
 <p>Don&apos;t forget your upcoming class!</p>
-
-<p>{{ $group }}</p>
-<p>{{ $dateTime }}</p>
-<p>{{ $location }}</p>
-<p>{{ $trainerName }}</p>
-<p>{{ $trainerEmail }}</p>
-<p>Transaction ID: {{ $transactionId }}</p>
+<p>Dont fortget your class <span class="blue-text">{{ $group->name }}</span> is coming up on the {{ date('M jS, g:ia', strtotime($dateTime)) }}</p>
+<p>This class iis held by <span class="blue-text">{{ $trainerName }}</span> </p>
+<p>Your transaction ID: <strong class="blue-text">{{ $transactionId }}</strong></p>
 
 <p>Take note of your unique booking code. Your trainer will need this - along with another form of ID - to identify you.</p>
 <p>Please try to arrive 15 minutes before the specified start time. Late arrivals can be rejected.</p>
@@ -22,8 +18,8 @@
 <p>Fitness is even more fun with friends! Click on the links below to share this class.</p>
 
 
-<a href="{{ Share::load(Request::url() , $group)->facebook()  }}" target="_blank"><span class="icon icon-fb-white mr20 hover">Facebook</span> </a>
-<a href="{{ Share::load(Request::url() , $group)->twitter()  }}" target="_blank"><span class="icon icon-twitter-white mr20 hover">Twitter</span> </a>
-<a href="{{ Share::load(Request::url() , $group)->gplus()  }}" target="_blank"><span class="icon icon-google-white hover"></span>Google Plus</a>
+<p><a href="{{ Share::load(Request::url() , $group)->facebook()  }}" target="_blank"><span class="blue-text">Facebook</span></a></p>
+<p><a href="{{ Share::load(Request::url() , $group)->twitter()  }}" target="_blank"><span class="blue-text">Twitter</span> </a></p>
+<p><a href="{{ Share::load(Request::url() , $group)->gplus()  }}" target="_blank"><span class="blue-text"></span>Google Plus</a></p>
 
 @stop
