395790d1774890e1712b2e35b52c3fcec1707b02
Lewis Bayfield <bassymew@gmail.com>
2014-12-17 17:15:57 +0000

    angular success message

--- app/views/v3/classes/discover/master.blade.php
+++ app/views/v3/classes/discover/master.blade.php
@@ -8,6 +8,16 @@
     @include('v3.landing.popup')
 
     @include('v3.angular.angular-view')
+    <!-- session alerts  -->
+    @if(Session::has('success'))
+        <div class="mt10 alert alert-success alert-dismissible fixed" role="alert">
+            {{ Session::get('success')  }}
+            <button type="button" class="close" data-dismiss="alert">
+                <span aria-hidden="true">&times;</span>
+                <span class="sr-only">Close</span>
+            </button>
+        </div>
+    @endif
 
 
     </div>
