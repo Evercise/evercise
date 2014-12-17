c792b768bdd756b06341a3002481d0081ff666ab
Tristan <tristangallen@gmail.com>
2014-12-17 18:44:01 +0000

    emails

--- app/views/v3/emails/trainer/registered.blade.php
+++ app/views/v3/emails/trainer/registered.blade.php
@@ -2,7 +2,7 @@
 <?php View::share('align', 'center') ?>
 
 @section('body')
-<p>It’s great to have you on board. We’re delighted you’ve decided to join the
+<p>It&apos;s great to have you on board. We’re delighted you’ve decided to join the
    Evercise Pay As You Go Fitness revolution and we reckon you’ll be just as
    thrilled with what Evercise can do for you! Your application is being
    processed but you can start creating classes straight away. This should take
@@ -10,6 +10,7 @@
    public.
 </p>
 <p>Thank you for your patience.</p>
-{{ Html::decode(Html::linkRoute('evercisegroups.create', image('/assets/img/email/btns/btn_get_started.png', 'Create your first class'), [$trainer->display_name], ['class' => 'btn btn-blue'])) }}
+
+{{ Html::decode(Html::linkRoute('evercisegroups.create', image('/assets/img/email/btns/btn_get_started.png', 'Create your first class'), [], ['class' => 'btn btn-blue'])) }}
 
 @stop
