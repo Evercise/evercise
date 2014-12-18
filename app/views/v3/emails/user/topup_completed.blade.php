ceb09483a28bad9ca33c3916893d9c65b42ac2b8
Lewis Bayfield <bassymew@gmail.com>
2014-12-18 13:22:07 +0000

    top up confo email

--- app/views/v3/emails/user/topup_completed.blade.php
+++ app/views/v3/emails/user/topup_completed.blade.php
@@ -11,41 +11,66 @@
 <p>Please keep this in a safe place for future reference.</p>
 @stop
 @section('extra')
-<table class="table" width="100%" height="20" align="left" cellspacing="30" cellpadding="0" bgcolor="#FFFFFF">
+<table class="table" width="60%" height="auto" align="left" cellspacing="10" cellpadding="0" bgcolor="#FFFFFF">
     <tbody>
         <tr>
-            <td colspan="7" align="right" >
-                <strong>Username: <span class="blue-text">{{ $user->display_name }}</span></strong>
+            <td width="20"></td>
+        </tr>
+
+        <tr>
+            <td  align="center" >
+                <strong>Username:</strong>
+            </td>
+            <td align="center">
+                <span class="blue-text">{{ $user->display_name }}</span>
             </td>
         </tr>
         <tr>
-            <td colspan="7" align="right" >
-                <strong>Full name: <span class="blue-text">{{ $user->first_name . ' ' . $user->last_name }}</span></strong>
+            <td align="center" >
+                <strong>Full name:</strong>
+            </td>
+            <td align="center" >
+                <span class="blue-text">{{ $user->first_name . ' ' . $user->last_name }}</span>
             </td>
         </tr>
         <tr>
-            <td colspan="7" align="right" >
-                <strong>Charge method: <span class="blue-text">{{ $transaction->payment_method }}</span></strong>
+            <td align="center" >
+                <strong>Charge method:</strong>
+            </td>
+            <td align="center" >
+                <span class="blue-text">{{ $transaction->payment_method }}</span>
             </td>
         </tr>
         <tr>
-            <td colspan="7" align="right" >
-                <strong>Date charged: <span class="blue-text">{{ $transaction->formattedDate() }}</span></strong>
+            <td align="center" >
+                <strong>Date charged:</strong>
+            </td>
+            <td align="center" >
+                <span class="blue-text">{{ $transaction->formattedDate() }}</span>
             </td>
         </tr>
         <tr>
-            <td colspan="7" align="right" >
-                <strong>Transaction ID: <span class="blue-text">{{ $transaction->id }}</span></strong>
+            <td center align="center" >
+                <strong>Transaction ID:</strong>
+            </td>
+            <td center align="center" >
+                 <span class="blue-text">{{ $transaction->id }}</span>
             </td>
         </tr>
         <tr>
-            <td colspan="7" align="right">
-                <strong>Funding amount: <span class="blue-text">&pound;{{abs($transaction->total_after_fees)}}</span></strong>
+            <td  align="center">
+                <strong>Funding amount:</strong>
+            </td>
+            <td  align="center">
+                <span class="blue-text">&pound;{{abs($transaction->total_after_fees)}}</span>
             </td>
         </tr>
         <tr>
-            <td colspan="7" align="right">
-                <strong>Wallet balance after funding: <span class="blue-text">&pound;{{$balance}}</span></strong>
+            <td align="center">
+                <strong>Wallet balance after funding:</strong>
+            </td>
+            <td align="center">
+                <span class="blue-text">&pound;{{$balance}}</span>
             </td>
         </tr>
     </tbody>
