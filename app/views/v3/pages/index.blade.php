184dc6aa5b38fda8adb69393c2cd786a1de06a41
Igor Matkovic <igor@cod3.me>
2014-12-18 11:25:09 +0000

    Link blog thumb and image

--- app/views/v3/pages/index.blade.php
+++ app/views/v3/pages/index.blade.php
@@ -14,11 +14,14 @@
              @foreach($articles as $a)
                 <div class="row" >
                     <div class="col-sm-4">
+
+                        <a href="{{ url(Articles::createUrl($a)) }}" alt="{{ $a->title }}">
                         {{ (!empty($a->thumb_image) ? image($a->thumb_image, $a->thumb_image, ['class' => 'img-responsive']) : image('files/articles/default_thumb.jpg', 'Image soon', ['class' => 'img-responsive'])) }}
+                        </a>
                     </div>
                     <div class="col-sm-8">
 
-                        <h3>{{ $a->title }}</h3>
+                        <h3><a href="{{ url(Articles::createUrl($a)) }}" alt="{{ $a->title }}">{{ $a->title }}</a></h3>
                         <p>{{ $a->intro }}</p>
                         <small>published on : {{ strtotime($a->published_on) != '-62169984000' ?  date('D dS Y',strtotime($a->published_on) ) :  date('D dS Y',strtotime($a->created_at) ) }}</small>
                         <br>
