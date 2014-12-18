6191c362bd949b4e8e9c6265e451f29f21ea7011
Igor Matkovic <igor@cod3.me>
2014-12-18 10:27:36 +0000

    Add Payments Cronjobs

---
+++ app/cronjobs/CheckPayments.php
@@ -0,0 +1,30 @@
+<?php namespace cronjobs;
+
+use Illuminate\Log\Writer;
+use Illuminate\Console\Application as Artisan;
+
+
+class DailyIndexer
+{
+
+    private $log;
+    private $artisan;
+
+    public function __construct(Writer $log, Artisan $artisan)
+    {
+
+        $this->log = $log;
+        $this->artisan = $artisan;
+    }
+
+    function run()
+    {
+        $this->log->info('Moving all sessions to payments');
+        $this->artisan->call('check:payments');
+
+        $this->log->info('Move Money to User Wallet');
+        $this->artisan->call('check:sessions');
+
+    }
+}
+
