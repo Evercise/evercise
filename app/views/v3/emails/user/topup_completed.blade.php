b4069f825b3a44aae2d24b8299f29ba5cc1d6381
Lewis Bayfield <bassymew@gmail.com>
2014-12-18 13:26:40 +0000

    topup again

--- app/views/v3/emails/user/topup_completed.blade.php
+++ app/views/v3/emails/user/topup_completed.blade.php
@@ -3,77 +3,15 @@
 
 
 @section('body')
-
 <p>Hi {{ $user->first_name }}</p>
-
 <p>Thank you for funding your Evercise wallet.</p>
 <p>You will find a copy of the transaction below.</p>
 <p>Please keep this in a safe place for future reference.</p>
-@stop
-@section('extra')
-<table class="table" width="60%" height="auto" align="left" cellspacing="10" cellpadding="0" bgcolor="#FFFFFF">
-    <tbody>
-        <tr>
-            <td width="20"></td>
-        </tr>
-
-        <tr>
-            <td  align="center" >
-                <strong>Username:</strong>
-            </td>
-            <td align="center">
-                <span class="blue-text">{{ $user->display_name }}</span>
-            </td>
-        </tr>
-        <tr>
-            <td align="center" >
-                <strong>Full name:</strong>
-            </td>
-            <td align="center" >
-                <span class="blue-text">{{ $user->first_name . ' ' . $user->last_name }}</span>
-            </td>
-        </tr>
-        <tr>
-            <td align="center" >
-                <strong>Charge method:</strong>
-            </td>
-            <td align="center" >
-                <span class="blue-text">{{ $transaction->payment_method }}</span>
-            </td>
-        </tr>
-        <tr>
-            <td align="center" >
-                <strong>Date charged:</strong>
-            </td>
-            <td align="center" >
-                <span class="blue-text">{{ $transaction->formattedDate() }}</span>
-            </td>
-        </tr>
-        <tr>
-            <td center align="center" >
-                <strong>Transaction ID:</strong>
-            </td>
-            <td center align="center" >
-                 <span class="blue-text">{{ $transaction->id }}</span>
-            </td>
-        </tr>
-        <tr>
-            <td  align="center">
-                <strong>Funding amount:</strong>
-            </td>
-            <td  align="center">
-                <span class="blue-text">&pound;{{abs($transaction->total_after_fees)}}</span>
-            </td>
-        </tr>
-        <tr>
-            <td align="center">
-                <strong>Wallet balance after funding:</strong>
-            </td>
-            <td align="center">
-                <span class="blue-text">&pound;{{$balance}}</span>
-            </td>
-        </tr>
-    </tbody>
-</table>
-
+<p><strong>Username: <span class="blue-text">{{ $user->display_name }}</span></strong></p>
+<p><strong>Full name: <span class="blue-text">{{ $user->first_name . ' ' . $user->last_name }}</span></strong></p>
+<p><strong>Charge method: <span class="blue-text">{{ $transaction->payment_method }}</span></strong></p>
+<p><strong>Date charged: <span class="blue-text">{{ $transaction->formattedDate() }}</span></strong></p>
+<p><strong>Transaction ID: <span class="blue-text">{{ $transaction->id }}</span></strong></p>
+<p><strong>Funding amount: <span class="blue-text">&pound;{{abs($transaction->total_after_fees)}}</span></strong></p>
+<p><strong>Wallet balance after funding: <span class="blue-text">&pound;{{$balance}}</span></strong></p>
 @stop
