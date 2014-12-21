f5773596d2810b0163841483c0fcb648d9b47077
Igor Matkovic <igor@cod3.me>
2014-12-21 14:51:42 +0000

    Fix Date and stripe

--- app/config/packages/abodeo/laravel-stripe/stripe.php
+++ app/config/packages/abodeo/laravel-stripe/stripe.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'api_key'         => (Config::get('evercise.stripe_testing') ? Config::get('evercise.stripe_api_key_test') : Config::get('evercise.stripe_api_key_test')),
-    'publishable_key' => (Config::get('evercise.stripe_testing') ? Config::get('evercise.stripe_pub_key_test') : Config::get('evercise.stripe_pub_key_test'))
+    'api_key'         => (Config::get('evercise.stripe_testing') ? Config::get('evercise.stripe_api_key_test') : Config::get('evercise.stripe_api_key')),
+    'publishable_key' => (Config::get('evercise.stripe_testing') ? Config::get('evercise.stripe_pub_key_test') : Config::get('evercise.stripe_pub_key'))
 ];
