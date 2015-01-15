6655b7c28c5df3dcf3331d9103e0d84f5ef24bae
Lewis Bayfield <bassymew@gmail.com>
2015-01-15 14:39:13 +0000

    prurchase complete

--- app/views/hello.php
+++ app/views/hello.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <style>h1 {font-family: helvetica, arial, sans-serif; text-transform: uppercase; font-size: 26px;  line-height: 30px; font-weight: 500; color:#768690; margin: 0}
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="content-type" content="text/html; charset=utf-8"><style>h1 {font-family: helvetica, arial, sans-serif; text-transform: uppercase; font-size: 26px;  line-height: 30px; font-weight: 500; color:#768690; margin: 0}
         h3 {font-family: helvetica, arial, sans-serif; text-transform: uppercase; font-size: 18px;  line-height: 26px; font-weight: 500; color:#768690; margin: 0}
         p { font-family: helvetica, arial, sans-serif; font-size: 14px;  line-height: 26px; font-weight: normal; color:#768690; }
         b { font-weight: bold; color:#768690; }
         table{ max-width: 640px; width: 100%; table-layout: fixed}
-        th{ font-family: helvetica, arial,  sans-serif; border-bottom:1px solid #ccc;  font-size: 18px;  line-height: 26px; font-weight: 500; color:#768690; margin: 0}
+        th{border-bottom: 1px solid #768690;  font-family: helvetica, arial, sans-serif;  font-size: 18px;  line-height: 26px; font-weight: 500; color:#768690; margin: 0}
         strong{ font-family: helvetica, arial, sans-serif;  font-size: 18px;  line-height: 26px; font-weight: 500; color:#768690; margin: 0}
         .container{ }
         .pink-text{ color: #ff1b7e}
@@ -46,163 +43,82 @@
             img { max-width: 640px; width: 100%}
             .footer { font-size: 13px; text-align: left;}
             .sm-hidden{ display: none}
-        }</style>
-</head>
-<body  bgcolor="#bfbfbf">
+        }</style></head><body bgcolor="#bfbfbf">
 <!-- Opening main -->
-<table  bgcolor="#FFFFFF" border="0" align="center" cellspacing="0" cellpadding="0">
-    <tr>
-        <td>
-            <table width="100%" height="auto" bgcolor="#FFFFFF" border="0" align="center" cellspacing="0" cellpadding="0">
-                <tr>
-                    <td>
+<table bgcolor="#FFFFFF" border="0" align="center" cellspacing="0" cellpadding="0" style="max-width: 640px; table-layout: fixed; width: 100%;"><tr><td>
+            <table width="100%" height="auto" bgcolor="#FFFFFF" border="0" align="center" cellspacing="0" cellpadding="0" style="max-width: 640px; table-layout: fixed; width: 100%;"><tr><td>
                         <!-- Main image -->
-                        <table width="100%" height="auto" border="0" cellspacing="0" cellpadding="0">
-                            <tr>
-                                <td>
+                        <table width="100%" height="auto" border="0" cellspacing="0" cellpadding="0" style="max-width: 640px; table-layout: fixed; width: 100%;"><tr><td>
                                     <a href="http://evercise.dev/uk/london" title="Confirmation of booking">
-                                        <img src="http://evercise.dev/assets/img/email/user_booking_confirmation.jpg" alt="booking confirmation">
-                                    </a>
+                                        <img src="http://evercise.dev/assets/img/email/user_booking_confirmation.jpg" alt="booking confirmation" style="width: 100%;"></a>
                                 </td>
-                            </tr>
-                        </table>
-                </tr>
-                </td>
-            </table>
-
-            <table width="100%" height="auto" border="0" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center">
-                <tr>
-                    <td>
+                            </tr></table></td></tr></table><table width="100%" height="auto" border="0" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="max-width: 640px; table-layout: fixed; width: 100%;"><tr><td>
                         <!-- Messaging -->
-                        <table width="100%" height="20" align="left" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF">
-                            <tr>
-                                <td>
+                        <table width="100%" height="20" align="left" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" style="max-width: 640px; table-layout: fixed; width: 100%;"><tr><td>
                                 </td>
-                            </tr>
-                        </table>
-                        <table width="100%" height="auto" align="left" cellspacing="30" cellpadding="0" bgcolor="#FFFFFF">
-                            <tr>
-                                <td align="left">
-                                    <p>Dear lewis_mewis</p>
-                                    <p>Thank for your Evercise booking! Please take note of your unique booking code (below). Your trainer will require this and another form of ID.</p>
-                                    <strong><p>Transaction ID: 5318074</p></strong>
+                            </tr></table><table width="100%" height="auto" align="left" cellspacing="30" cellpadding="0" bgcolor="#FFFFFF" style="max-width: 640px; table-layout: fixed; width: 100%;"><tr><td align="left">
+
+                                    <p style="color: #768690; font-family: helvetica, arial, sans-serif; font-size: 14px; font-weight: normal; line-height: 26px;">Dear lewis_mewis</p>
+                                    <p style="color: #768690; font-family: helvetica, arial, sans-serif; font-size: 14px; font-weight: normal; line-height: 26px;">Thank for your Evercise booking! Please take note of your unique booking code (below). Your trainer will require this and another form of ID.</p>
+                                    <strong style="color: #768690; font-family: helvetica, arial, sans-serif; font-size: 18px; font-weight: 500; line-height: 26px; margin: 0;"><p style="color: #768690; font-family: helvetica, arial, sans-serif; font-size: 14px; font-weight: normal; line-height: 26px;">Transaction ID: 5318080</p></strong>
                                 </td>
-                            </tr>
-                        </table>
-                        <table width="100%" height="auto" align="left" cellspacing="0" cellpadding="30" bgcolor="#FFFFFF">
-                            <tr>
-                                <td>
-                                    <table class="table" width="100%" height="auto" align="left"  cellspacing="0" cellpadding="0" bgcolor="#FFFFFF">
-                                        <tbody>
-                                        <tr align="left" height="50">
-                                            <th colspan="2">
+                            </tr></table><table class="table" width="100%" height="auto" align="left" cellspacing="0" cellpadding="30" bgcolor="#FFFFFF" style="max-width: 640px; table-layout: fixed; width: 100%;"><tbody><tr><th style="border-bottom: 1px solid #768690; color: #768690; font-family: helvetica, arial, sans-serif; font-size: 18px; font-weight: 500; line-height: 26px; margin: 0;">
+                                    <table class="table" width="100%" height="auto" align="left" cellspacing="" cellpadding="0" bgcolor="#FFFFFF" style="max-width: 640px; table-layout: fixed; width: 100%;"><tbody><tr align="left"><th colspan="2" style="border-bottom: 1px solid #768690; color: #768690; font-family: helvetica, arial, sans-serif; font-size: 18px; font-weight: 500; line-height: 26px; margin: 0;">
                                                 Name
                                             </th>
-                                            <th colspan="3">
+                                            <th colspan="3" style="border-bottom: 1px solid #768690; color: #768690; font-family: helvetica, arial, sans-serif; font-size: 18px; font-weight: 500; line-height: 26px; margin: 0;">
                                                 Date
                                             </th>
-
-                                            <th>
+                                            <th style="border-bottom: 1px solid #768690; color: #768690; font-family: helvetica, arial, sans-serif; font-size: 18px; font-weight: 500; line-height: 26px; margin: 0;">
                                                 Price
                                             </th>
-                                            <th>
+                                            <th style="border-bottom: 1px solid #768690; color: #768690; font-family: helvetica, arial, sans-serif; font-size: 18px; font-weight: 500; line-height: 26px; margin: 0;">
                                                 Code
                                             </th>
-                                        </tr>
-                                        <tr  align="left">
-                                            <td colspan="2" valign="top">
-                                                <p><a href="http://evercise.dev/classes/reading-test" class="blue-text">reading test</a> x2</p>
+                                        </tr><tr align="left"><td colspan="2">
+                                                <p style="color: #768690; font-family: helvetica, arial, sans-serif; font-size: 14px; font-weight: normal; line-height: 26px;"><a href="http://evercise.dev/classes/reading-test" class="blue-text" style="color: #5ECDE8;">reading test</a></p>
                                             </td>
-                                            <td  colspan="3" valign="top">
-                                                <p>Sat, Jan 31, 2015 12:00 PM</p>
+                                            <td colspan="3">
+                                                <p style="color: #768690; font-family: helvetica, arial, sans-serif; font-size: 14px; font-weight: normal; line-height: 26px;">Sat, Jan 31, 2015 12:00 PM</p>
                                             </td>
-                                            <td valign="top">
-                                                <p>&pound;20</p>
+                                            <td>
+                                                <p style="color: #768690; font-family: helvetica, arial, sans-serif; font-size: 14px; font-weight: normal; line-height: 26px;">£10.00</p>
                                             </td>
-                                            <td valign="top">
-                                                <p>8smh83, 8smh83</p>
+                                            <td>
+                                                <p style="color: #768690; font-family: helvetica, arial, sans-serif; font-size: 14px; font-weight: normal; line-height: 26px;">
+                                                    8smhoz<br></p>
                                             </td>
-                                        </tr>
-                                        <tr align="left">
-                                            <td colspan="2" valign="top">
-                                                <p><a href="http://evercise.dev/classes/reading-test" class="blue-text">reading test</a> x2</p>
+                                        </tr><tr><td colspan="7" align="left">
+                                                <br><br><strong style="color: #768690; font-family: helvetica, arial, sans-serif; font-size: 18px; font-weight: 500; line-height: 26px; margin: 0;">Sub-total <span class="blue-text" style="color: #5ECDE8;">£10.00</span></strong>
+                                                <br><br></td>
+                                        </tr><tr><td colspan="7" align="left">
+                                                <strong style="color: #768690; font-family: helvetica, arial, sans-serif; font-size: 18px; font-weight: 500; line-height: 26px; margin: 0;">Total <span class="blue-text" style="color: #5ECDE8;">£10.00</span></strong>
                                             </td>
-                                            <td  colspan="3" valign="top">
-                                                <p>Sat, Jan 31, 2015 12:00 PM</p>
-                                            </td>
-                                            <td valign="top">
-                                                <p>&pound;20</p>
-                                            </td>
-                                            <td valign="top">
-                                                <p>8smh83</p>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="7" align="">
-                                                <br>
-                                                <br>
-                                                <strong>Sub-total <span class="blue-text">&pound;20</span></strong>
-                                                <br>
-                                                <br>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="7" align="">
-                                                <strong>Total <span class="blue-text">&pound;20</span></strong>
-                                            </td>
+                                        </tr></tbody></table></th>
+                            </tr></tbody></table><!-- Footer --><table width="100%" height="85" cellspacing="0" cellpadding="0" bgcolor="#ffffff" style="max-width: 640px; table-layout: fixed; width: 100%;"><tr><td class="sm-hidden" width="30" height="85"></td>
 
-                                        </tr>
-                                        </tbody>
-                                    </table>
-                                </td>
-                            </tr>
-                        </table>
+                                <td class="sm-hidden" height="50">
 
-
-
-                        <!-- Footer -->
-                        <table width="100%" height="85" cellspacing="0" cellpadding="0" bgcolor="#ffffff">
-                            <tr>
-                                <td class="sm-hidden" width="30"  height="85"></td>
-
-                                <td class="sm-hidden"  height="50">
-
-                                    <p class="footer">&copy; Copyright 2014 Evercise</p>
+                                    <p class="footer" style="color: #768690; font-family: helvetica, arial, sans-serif; font-size: 12px; font-weight: normal; line-height: 26px; text-align: left;">© Copyright 2014 Evercise</p>
 
                                 </td>
-                                <td width="100"  height="40" align="center">
-                                    <p>Follow us on</p>
-                                </td>
-                                <td width="40"  height="40" align="center">
-                                    <a href="http://facebook.com/evercise"><img src="http://evercise.com/img/email-campaign/facebook.png" width="40" height="40" border="0" alt="Facebook"></a>
+                                <td width="100" height="40" align="center">
+                                    <p style="color: #768690; font-family: helvetica, arial, sans-serif; font-size: 14px; font-weight: normal; line-height: 26px;">Follow us on</p>
                                 </td>
                                 <td width="40" height="40" align="center">
-                                    <a href="http://twitter.com/evercise"><img src="http://evercise.com/img/email-campaign/twitter.png" width="40" height="40" border="0" alt="Twitter"></a>
+                                    <a href="http://facebook.com/evercise"><img src="http://evercise.com/img/email-campaign/facebook.png" width="40" height="40" border="0" alt="Facebook" style="width: 100%;"></a>
                                 </td>
                                 <td width="40" height="40" align="center">
-                                    <a href="http://instagram.com/evercisefitness"><img src="http://evercise.com/img/email-campaign/instagram.png" width="40" height="40" border="0" alt="Instagram"></a>
+                                    <a href="http://twitter.com/evercise"><img src="http://evercise.com/img/email-campaign/twitter.png" width="40" height="40" border="0" alt="Twitter" style="width: 100%;"></a>
+                                </td>
+                                <td width="40" height="40" align="center">
+                                    <a href="http://instagram.com/evercisefitness"><img src="http://evercise.com/img/email-campaign/instagram.png" width="40" height="40" border="0" alt="Instagram" style="width: 100%;"></a>
                                 </td>
 
                                 <td class="sm-hidden" width="30" height="85"></td>
-                            </tr>
-                        </table>
-                        <table width="100%" height="20" cellspacing="0" cellpadding="30" bgcolor="#ffffff">
-                            <tr>
-                                <td>
-                                    %%unsubscribe%%
+                            </tr></table><table width="100%" height="20" cellspacing="0" cellpadding="30" bgcolor="#ffffff" style="max-width: 640px; table-layout: fixed; width: 100%;"><tr><td>
+
                                 </td>
-                            </tr>
-                        </table>
-
-
-
-                        <!-- Closing main -->
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-</table>
-</body>
-</html>
-
+                            </tr></table><!-- Closing main --></td>
+                </tr></table></td>
+    </tr></table></body></html>
--- app/views/v3/emails/user/cart_completed_class.blade.php
+++ app/views/v3/emails/user/cart_completed_class.blade.php
@@ -9,97 +9,107 @@
 <strong><p>Transaction ID: {{$transaction->id}}</p></strong>
 @stop
 @section('extra')
-<table class="table" width="100%" height="20" align="left" cellspacing="30" cellpadding="0" bgcolor="#FFFFFF">
+<table class="table" width="100%" height="auto" align="left" cellspacing="0" cellpadding="30" bgcolor="#FFFFFF">
     <tbody>
-        <tr align="left">
-            <th colspan="2">
-                Name
-            </th>
-            <th colspan="3">
-                Date
-            </th>
+        <tr>
             <th>
-                Price
-            </th>
-            <th>
-                Code
-            </th>
-        </tr>
-        @foreach($cart['sessions_grouped'] as $sessionId => $row)
-            <?php
-                $date = new \Carbon\Carbon($row['date_time']);
-            ?>
-            <tr  align="left">
-                <td colspan="2">
-                    <p>{{ Html::linkRoute('class.show', $row['name'], [Evercisegroup::getSlug( $row['evercisegroup_id'] ) ], ['class' => 'blue-text'] ) }}</p>
-                </td>
-                <td  colspan="3">
-                    <p>{{ $date->toDayDateTimeString() }}</p>
-                </td>
-                <td>
-                    <p>{{ ($row['grouped_price_discount'] != $row['grouped_price'] ? '<strike>&pound;'.$row['grouped_price'].'</strike> &pound;'.$row['grouped_price_discount'] : '&pound;'.$row['grouped_price']) }}</p>
-                </td>
-                <td>
-                    <p>
-                        @foreach($transaction->makeBookingHashBySession($sessionId) as $hash)
-                            {{ $hash }}<br>
+                <table class="table" width="100%" height="auto" align="left" cellspacing="" cellpadding="0" bgcolor="#FFFFFF">
+                    <tbody>
+                        <tr align="left" height="50">
+                            <th colspan="2">
+                                Name
+                            </th>
+                            <th colspan="3">
+                                Date
+                            </th>
+                            <th>
+                                Price
+                            </th>
+                            <th>
+                                Code
+                            </th>
+                        </tr>
+                        @foreach($cart['sessions_grouped'] as $sessionId => $row)
+                            <?php
+                                $date = new \Carbon\Carbon($row['date_time']);
+                            ?>
+                            <tr  align="left">
+                                <td colspan="2">
+                                    <p>{{ Html::linkRoute('class.show', $row['name'], [Evercisegroup::getSlug( $row['evercisegroup_id'] ) ], ['class' => 'blue-text'] ) }}</p>
+                                </td>
+                                <td  colspan="3">
+                                    <p>{{ $date->toDayDateTimeString() }}</p>
+                                </td>
+                                <td>
+                                    <p>{{ ($row['grouped_price_discount'] != $row['grouped_price'] ? '<strike>&pound;'.$row['grouped_price'].'</strike> &pound;'.$row['grouped_price_discount'] : '&pound;'.$row['grouped_price']) }}</p>
+                                </td>
+                                <td>
+                                    <p>
+                                        @foreach($transaction->makeBookingHashBySession($sessionId) as $hash)
+                                            {{ $hash }}<br>
+                                        @endforeach
+                                    </p>
+                                </td>
+                            </tr>
                         @endforeach
-                    </p>
-                </td>
-            </tr>
-        @endforeach
-        @if(isset($cart['packages']))
-            @foreach($cart['packages'] as $row)
-                <tr  align="left">
-                    <td colspan="2">
-                        <p>{{ $row['name'] }}</p>
-                    </td>
-                    <td  colspan="2">
-                        <p>{{ $row['classes'] }} classes</p>
-                    </td>
-                    <td>
+                        @if(isset($cart['packages']))
+                            @foreach($cart['packages'] as $row)
+                                <tr  align="left">
+                                    <td colspan="2">
+                                        <p>{{ $row['name'] }}</p>
+                                    </td>
+                                    <td  colspan="2">
+                                        <p>{{ $row['classes'] }} classes</p>
+                                    </td>
+                                    <td>
 
-                    </td>
-                    <td>
-                    <td>
-                        <p>1</p>
-                    </td>
-                    <td>
-                        <p>&pound;{{ $row['price'] }}</p>
-                    </td>
-                </tr>
-            @endforeach
-        @endif
-        <tr>
-            <td colspan="7" >
-                <strong>Sub-total <span class="blue-text">&pound;{{ number_format($cart['total']['subtotal'], 2) }}</span></strong>
-                @if($cart['total']['package_deduct'] > 0)
-                    <strong>Package deduct: <span class="blue-text"> &pound;{{ number_format($cart['total']['package_deduct'], 2)  }}</span></strong>
-                    <br/>
-                @endif
-                @if($cart['total']['from_wallet'] > 0)
-                    <strong>From Wallet: <span class="blue-text"> &pound;{{ number_format($cart['total']['from_wallet'], 2)  }}</span></strong>
-                    <br/>
-                @endif
-                @if(!empty($cart['discount']['amount']) && $cart['discount']['amount'] > 0)
-                    <strong>
-                        Voucher discount: <span class="blue-text">-  &pound;{{ number_format($cart['discount']['amount'], 2) }}</span>
-                         @if($cart['discount']['type'] == 'percentage')
-                             <span class="blue-text">{{ $cart['discount']['percentage']}}%</span>
-                         @endif
-                    </strong>
-                @endif
-            </td>
+                                    </td>
+                                    <td>
+                                    <td>
+                                        <p>1</p>
+                                    </td>
+                                    <td>
+                                        <p>&pound;{{ $row['price'] }}</p>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @endif
+                        <tr>
+                            <td colspan="7" align="left">
+                                <br>
+                                <br>
+                                <strong>Sub-total <span class="blue-text">&pound;{{ number_format($cart['total']['subtotal'], 2) }}</span></strong>
+                                @if($cart['total']['package_deduct'] > 0)
+                                    <strong>Package deduct: <span class="blue-text"> &pound;{{ number_format($cart['total']['package_deduct'], 2)  }}</span></strong>
+                                    <br/>
+                                @endif
+                                @if($cart['total']['from_wallet'] > 0)
+                                    <strong>From Wallet: <span class="blue-text"> &pound;{{ number_format($cart['total']['from_wallet'], 2)  }}</span></strong>
+                                    <br/>
+                                @endif
+                                @if(!empty($cart['discount']['amount']) && $cart['discount']['amount'] > 0)
+                                    <strong>
+                                        Voucher discount: <span class="blue-text">-  &pound;{{ number_format($cart['discount']['amount'], 2) }}</span>
+                                         @if($cart['discount']['type'] == 'percentage')
+                                             <span class="blue-text">{{ $cart['discount']['percentage']}}%</span>
+                                         @endif
+                                    </strong>
+                                @endif
+                                <br>
+                                <br>
+                            </td>
+                        </tr>
+                        @if($cart['total']['final_cost'] > 0)
+                        <tr>
+                            <td colspan="7" align="left">
+                                <strong>Total <span class="blue-text">&pound;{{number_format($cart['total']['final_cost'], 2)}}</span></strong>
+                            </td>
+                        </tr>
+                        @endif
+                    </tbody>
+                </table>
+            </th>
         </tr>
-        @if($cart['total']['final_cost'] > 0)
-        <tr>
-            <td colspan="7" align="right">
-                <strong>Total <span class="blue-text">&pound;{{number_format($cart['total']['final_cost'], 2)}}</span></strong>
-            </td>
-
-        </tr>
-        @endif
     </tbody>
 </table>
-
 @stop
