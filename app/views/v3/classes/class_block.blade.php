dd14e2438378e8d8600506136a048555a74153c9
Lewis Bayfield <bassymew@gmail.com>
2015-02-09 12:22:48 +0000

    class block links

--- app/views/v3/classes/class_block.blade.php
+++ app/views/v3/classes/class_block.blade.php
@@ -1,12 +1,12 @@
 <ul class="list-group class-block ">
     <li class="list-group-item class-img-wrapper">
-        {{ image($result->user->directory.'/preview_'.$result->image, $result->image, ['class' => 'img-responsive']) }}
+        {{ Html::decode( Html::linkRoute('class.show', image($result->user->directory.'/preview_'.$result->image, $result->image, ['class' => 'img-responsive']) , $result->slug ) ) }}
     </li>
     <div class="class-body">
-        {{ image($result->user->directory.'/search_'.$result->image, $result->image, ['class' => 'img-responsive']) }}
+        {{ Html::decode( Html::linkRoute('class.show', image($result->user->directory.'/preview_'.$result->image, $result->image, ['class' => 'img-responsive']) , $result->slug ) ) }}
         <li class="list-group-item text-center class-title">
-            <h4>{{ str_limit($result->name, 25, '...')  }}</h4>
-            <p><span class="icon icon-sm icon-sm-marker mr5"></span>{{ $result->venue->town }}</p>
+            <h4>{{ Html::decode( Html::linkRoute('class.show',str_limit($result->name, 25, '...'), $result->slug ) ) }}</h4>
+            <p><span class="icon icon-sm icon-sm-marker mr5"></span>{{ str_limit($result->venue->address, 24, '...') }}</p>
         </li>
         <li class="list-group-item class-footer">
             <aside class="text-center"><strong class="text-primary">£{{ round($result->default_price ,2)}}</strong></aside>
